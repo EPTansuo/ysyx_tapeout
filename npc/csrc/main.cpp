09483a2ac14376d1ac50b5b674a8d1658cedfa15
EPTansuo <eptansuo@163.com>
2024-01-05 18:54:25 +0800

    verilator example

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,37 @@
-#include <stdio.h>
+#include "../obj_dir/Vexample.h"
+#include "verilated.h" 
+#include <iostream>
+#include <verilated_vcd_c.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+
+int main(int argc, char**argv)
+{
+	Verilated::commandArgs(argc, argv); 
+	Verilated::traceEverOn(true);
+	Vexample* top = new Vexample;      
+	
+	VerilatedVcdC *tfp = new VerilatedVcdC;
+	
+	top->trace(tfp,0);
+	tfp->open("wave.vcd");
+
+	for (int i = 0; i < 4; i++) {
+		top->a = i & 1;                
+		top->b = (i >> 1) & 1;         
+
+		top->eval();                   
+		tfp->dump(i);
+
+		std::cout << "a = " << (top->a ? "1" : "0") 
+			<< ", b = " << (top->b ? "1" : "0")
+			<< ", f = " << (top->f ? "1" : "0") << std::endl;
+
+
+		}
+	
+	top->final();
+	tfp->close();
+	
+	delete top;
+	
 }
