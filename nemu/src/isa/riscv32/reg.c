151fecc7c5e14779cc7c77e8dc8b7e02e253e520
tracer-ysyx <tracer@ysyx.org>
2024-03-14 12:54:38 +0800

    > compile NEMU ysyx_22040000 韩炳晋 Linux archlinux 6.6.21-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 06 Mar 2024 16:59:55 +0000 x86_64 GNU/Linux  12:54:38 up 1 day, 14:41,  3 users,  load average: 4.74, 5.04, 4.24

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -26,6 +26,11 @@ const char *regs[] = {
 
 void isa_reg_display() {
 	printf("func: isa_reg_display");
+  uint32_t reg_num = sizeof(regs) / sizeof(regs[0]);
+  printf("%ld",sizeof(cpu.gpr)/sizeof(cpu.gpr[0]));
+  for (int i = 0; i < reg_num; i++) {
+        //printf("reg$%s = 0x%ld\n", regs[i], cpu.gpr[i]);
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -63,6 +63,9 @@ static int cmd_si(char * args){
   return 0;
 }
 
+static int cmd_info(char *args){
+  return 0;
+}
 
 static int cmd_help(char *args);
 
@@ -78,10 +81,11 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  {"info", "Print the information", cmd_info},
 
   /* TODO: Add more commands */
   {"si","Single step", cmd_si},
-  
+
 
 };
 
