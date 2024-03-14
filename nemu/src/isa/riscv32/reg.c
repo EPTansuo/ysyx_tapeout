3d8502dfe9f76952a1d2026abae9769d09d5c390
tracer-ysyx <tracer@ysyx.org>
2024-03-14 09:53:13 +0800

    > compile NEMU ysyx_22040000 韩炳晋 Linux archlinux 6.6.21-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 06 Mar 2024 16:59:55 +0000 x86_64 GNU/Linux  09:53:13 up 1 day, 11:40,  3 users,  load average: 5.19, 4.21, 3.41

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -15,6 +15,7 @@
 
 #include <isa.h>
 #include "local-include/reg.h"
+#include <stdio.h>
 
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
@@ -24,6 +25,7 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+	printf("func: isa_reg_display");
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
