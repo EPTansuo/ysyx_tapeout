9a41f91098507a21d272d6826500628d21354633
tracer-ysyx <tracer@ysyx.org>
2024-03-14 13:15:23 +0800

    > compile NEMU ysyx_22040000 韩炳晋 Linux archlinux 6.6.21-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 06 Mar 2024 16:59:55 +0000 x86_64 GNU/Linux  13:15:23 up 1 day, 15:02,  3 users,  load average: 5.07, 4.93, 4.64

--- nemu/src/isa/riscv32/local-include/reg.h
+++ nemu/src/isa/riscv32/local-include/reg.h
@@ -18,6 +18,8 @@
 
 #include <common.h>
 
+#undef CONFIG_RVE 
+
 static inline int check_reg_idx(int idx) {
   IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < MUXDEF(CONFIG_RVE, 16, 32)));
   return idx;
