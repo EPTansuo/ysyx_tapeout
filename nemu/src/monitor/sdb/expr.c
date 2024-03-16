db4bf1eb8f658e6932cc8970aaa84fd651d5ac98
tracer-ysyx <tracer@ysyx.org>
2024-03-16 15:05:28 +0800

    > compile NEMU ysyx_22040000 韩炳晋 Linux archlinux 6.6.21-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 06 Mar 2024 16:59:55 +0000 x86_64 GNU/Linux  15:05:28 up 1 day, 10 min,  2 users,  load average: 0.62, 0.96, 1.04

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -324,6 +324,10 @@ uint32_t eval(int p, int q) {
       case '-': return val1 - val2;
       case '*': return val1 * val2;
       case '/': return (val2 != 0 ? (val1 / val2) : 0);
+      case TK_EQ:  return (val1 == val2);
+      case TK_AND: return (val1 && val2);
+      case TK_OR:  return (val1 || val2);
+      case TK_NOT: return (!val2);
       default: assert(0);
     }
   }
