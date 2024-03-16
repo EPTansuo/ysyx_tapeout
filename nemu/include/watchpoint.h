9f0c3914c784f625328a528241595bd0c7344cd3
tracer-ysyx <tracer@ysyx.org>
2024-03-16 17:44:11 +0800

    > compile NEMU ysyx_22040000 韩炳晋 Linux archlinux 6.6.21-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 06 Mar 2024 16:59:55 +0000 x86_64 GNU/Linux  17:44:11 up 1 day,  2:48,  2 users,  load average: 1.06, 1.50, 1.40

--- nemu/include/watchpoint.h
+++ nemu/include/watchpoint.h
@@ -1,6 +1,22 @@
 #ifndef __WATCHPOINT_H_
 #define __WATCHPOINT_H_
 
+#include <common.h>
+
+#define NR_WP 32
+
+
+
+typedef struct watchpoint {
+  int NO;
+  struct watchpoint *next;
+
+  /* TODO: Add more members if necessary */
+  char expr[256];
+  word_t value;
+  word_t value_old;
+} WP;
+
 
 void set_watchpoint(char *e);
 void del_watchpoint(int NO);
--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -18,19 +18,6 @@
 
 
 
-#define NR_WP 32
-
-
-
-typedef struct watchpoint {
-  int NO;
-  struct watchpoint *next;
-
-  /* TODO: Add more members if necessary */
-  char expr[256];
-  word_t value;
-  word_t value_old;
-} WP;
 
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
