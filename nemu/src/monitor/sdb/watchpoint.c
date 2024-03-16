7387c455af9ae314909e507a9b8ab9d6bac90d9e
tracer-ysyx <tracer@ysyx.org>
2024-03-16 18:30:56 +0800

    > compile NEMU ysyx_22040000 韩炳晋 Linux archlinux 6.6.21-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 06 Mar 2024 16:59:55 +0000 x86_64 GNU/Linux  18:30:56 up 1 day,  3:35,  2 users,  load average: 1.20, 1.26, 1.08

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -131,8 +131,11 @@ void scan_watchpoint(){
      if(result != p->value_old){
       nemu_state.state = NEMU_STOP;
       printf("Watchpoint %d: %s\n", p->NO, p->expr);
-      printf("Old value = %lu  %016lx\n", p->value_old,p->value_old);
-      printf("New value = %lu  %016lx\n", p->value,p->value_old);
+      printf("Old value = %lu  0x%016lx\n", p->value_old,p->value_old);
+      printf("New value = %lu  0x%016lx\n",result,result);
+
+      p->value_old = p->value;
+      p->value = result;
      }
   }
 }
