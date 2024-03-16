ca1365eb9407cdc4fbb31c232af5b7485af0ea3c
tracer-ysyx <tracer@ysyx.org>
2024-03-16 09:41:00 +0800

    > compile NEMU ysyx_22040000 韩炳晋 Linux archlinux 6.6.21-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 06 Mar 2024 16:59:55 +0000 x86_64 GNU/Linux  09:41:00 up 18:45,  2 users,  load average: 1.00, 1.06, 1.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -117,12 +117,13 @@ static bool make_token(char *e) {
 
   while (e[position] != '\0') {
     /* Try all rules one by one. */
-    for (i = 0; i < NR_REGEX; i ++) {
+    for (i = 0; i < NR_REGEX; i++) {
       //匹配成功 && 匹配的字符串正好是当前开始的位置
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
+        printf("ENTER IF\n");
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
