ee5041138929393a96c0174c57520b6b230f64aa
tracer-ysyx <tracer@ysyx.org>
2024-03-21 10:11:54 +0800

    > sim RTL ysyx_22040000 韩炳晋 Linux archlinux 6.6.22-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 16 Mar 2024 06:20:33 +0000 x86_64 GNU/Linux  10:11:54 up 15:47,  1 user,  load average: 0.94, 1.04, 1.20

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,37 +1,65 @@
 #include "../obj_dir/Vexample.h"
-#include "verilated.h" 
+#include "verilated.h"
 #include <iostream>
 #include <verilated_vcd_c.h>
+#include <nvboard.h>
 
+static TOP_NAME dut;
 
-int main(int argc, char**argv)
+void nvboard_bind_all_pins(TOP_NAME *top);
+
+static void single_cycle()
 {
-	Verilated::commandArgs(argc, argv); 
+	//dut.clk = 0;
+	dut.eval();
+	//dut.clk = 1;
+	dut.eval();
+}
+
+
+void verilator_sim(int argc, char **argv)
+{
+	Verilated::commandArgs(argc, argv);
 	Verilated::traceEverOn(true);
-	Vexample* top = new Vexample;      
-	
+	Vexample *top = new Vexample;
+
 	VerilatedVcdC *tfp = new VerilatedVcdC;
-	
-	top->trace(tfp,0);
+
+	top->trace(tfp, 0);
 	tfp->open("wave.vcd");
 
-	for (int i = 0; i < 10; i++) {
-		top->a = i & 1;                
-		top->b = (i >> 1) & 1;         
+	for (int i = 0; i < 10; i++)
+	{
+		top->a = i & 1;
+		top->b = (i >> 1) & 1;
 
-		top->eval();                   
+		top->eval();
 		tfp->dump(i);
 
-		std::cout << "a = " << (top->a ? "1" : "0") 
-			<< ", b = " << (top->b ? "1" : "0")
-			<< ", f = " << (top->f ? "1" : "0") << std::endl;
+		std::cout << "a = " << (top->a ? "1" : "0")
+			  << ", b = " << (top->b ? "1" : "0")
+			  << ", f = " << (top->f ? "1" : "0") << std::endl;
+	}
 
-
-		}
-	
 	top->final();
 	tfp->close();
-	
+
 	delete top;
-	
+}
+
+void nvboard_sim()
+{
+}
+
+int main(int argc, char **argv)
+{
+	nvboard_bind_all_pins(&dut);
+	nvboard_init();
+
+
+	while (1)
+	{
+		nvboard_update();
+		single_cycle();
+	}
 }
