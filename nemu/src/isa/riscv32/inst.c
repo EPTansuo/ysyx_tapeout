d638b9e4ca8917c5576023b621d8ea6aec75b2f4
tracer-ysyx <tracer@ysyx.org>
2024-03-29 18:41:59 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.22-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 16 Mar 2024 06:20:33 +0000 x86_64 GNU/Linux  18:41:59 up  4:41,  1 user,  load average: 1.66, 1.32, 1.02

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -59,7 +59,7 @@ static int decode_exec(Decode *s) {
   __VA_ARGS__ ; \
 }
 
-  printf("s->pc: %ld\n",s->pc);
+  printf("s->pc: 0x%lx\n",s->pc);
 
   INSTPAT_START();
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
