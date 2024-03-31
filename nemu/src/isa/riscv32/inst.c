d97fadd9d8abc791d490938b839fd06d2039cdcb
tracer-ysyx <tracer@ysyx.org>
2024-03-31 11:04:15 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.22-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 16 Mar 2024 06:20:33 +0000 x86_64 GNU/Linux  11:04:15 up 1 day, 21:03,  1 user,  load average: 1.08, 0.93, 0.78

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -32,14 +32,14 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-/*q#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | BITS(i, 30, 21) << 1 | \
+#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | BITS(i, 30, 21) << 1 | \
     (BITS(i, 20, 20) << 11) | (BITS(i, 19, 12) << 12) ; } while(0)
+
+/*
 #define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 7, 7) << 11) | \
     (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1);} while(0)
 */
-#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | BITS(i, 30, 21) << 1 \
-                          | BITS(i, 20, 20) << 11 | BITS(i, 19, 12) << 12 ; } while(0)
-                          
+
 #define immB() do { *imm = SEXT(BITS(i, 31, 31), 1) << 11 | ((SEXT(BITS(i, 7, 7), 1) << 63) >> 63) << 10 | ((SEXT(BITS(i, 30, 25), 6) << 58) >> 58) << 4 | ((SEXT(BITS(i, 11, 8), 4) << 60) >> 60); *imm = *imm << 1; } while (0)
 
 
