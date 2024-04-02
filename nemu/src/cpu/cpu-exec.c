da45ce710742b43feea807c954102df5417665db
tracer-ysyx <tracer@ysyx.org>
2024-04-02 15:55:25 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.23-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 27 Mar 2024 07:47:20 +0000 x86_64 GNU/Linux  15:55:25 up  6:15,  1 user,  load average: 1.14, 1.85, 2.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -51,9 +51,9 @@ void print_iringbuf(){
     else
       printf("    ");
     printf("0x%016lx: ", iringbuf.pc[i]);
-    for(int j=0; i<4; i++){
-      printf("%02x ", (iringbuf.inst[i] >> (8 * j)) & 0xff);
-    }
+    //for(int j=0; i<4; i++){
+   //   printf("%02x ", (iringbuf.inst[i] >> (8 * j)) & 0xff);
+    //}
     disassemble(logbuf, 128, iringbuf.pc[i] , (uint8_t*)(&(iringbuf.inst[i])), 4);
     printf("%s\n", logbuf);
   }
