e229f15515c706dff9ea717f873cbf77addebed2
tracer-ysyx <tracer@ysyx.org>
2024-04-02 16:11:35 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.23-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 27 Mar 2024 07:47:20 +0000 x86_64 GNU/Linux  16:11:35 up  6:31,  1 user,  load average: 2.54, 2.24, 2.24

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -25,7 +25,7 @@
  * You can modify this value as you want.
  */
 #define MAX_INST_TO_PRINT 1001
-#define IRINGBUF_SIZE 16
+#define IRINGBUF_SIZE 20
 
 CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
