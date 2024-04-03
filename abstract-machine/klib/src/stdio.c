7981a804409ce03ea7c12908b9afe3d080c40a23
tracer-ysyx <tracer@ysyx.org>
2024-04-03 16:29:33 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.23-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 27 Mar 2024 07:47:20 +0000 x86_64 GNU/Linux  16:29:33 up  3:54,  1 user,  load average: 4.01, 3.82, 3.95

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,6 +2,7 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
+#include <string.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
@@ -9,12 +10,74 @@ int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
+int itoa(char* dst, int num){
+  int digits = 0;
+  int bits = 0;
+  if (num == 0) {
+    dst[digits++] = '0';
+        bits = 1;
+  } 
+  else {
+    if (num < 0) {
+      dst[digits++] = '-';
+      num = -num;
+    }
+    int temp = num;
+    while (temp != 0) {   //判断有几位数
+      temp /= 10;
+      bits++;
+          digits++;
+    }
+    temp = num;   
+    while (temp != 0) {   
+      dst[--digits] = '0' + (temp % 10);
+      temp /= 10;
+    }
+  }
+  dst[digits+bits] = '\0';
+  printf("iota:dst:%s\n",dst);
+  return digits+bits;
+}
+
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int len = 0;
+  char buf[256]; 
+
+  while (*fmt) {
+    if (*fmt == '%') {
+      fmt++; // 跳过 '%'
+      if (*fmt == 'd') {
+        int num = va_arg(ap, int);
+        int digits = itoa(buf, num);
+        for (int i = 0; i < digits; i++) {
+          *out++ = buf[i];
+          len++;
+        }
+      } else if (*fmt == 's') {
+        const char *str = va_arg(ap, const char *);
+        int str_len = strlen(str);
+        for (int i = 0; i < str_len; i++) {
+          *out++ = str[i];
+          len++;
+        }
+      }
+    } else {
+      *out++ = *fmt;
+      len++;
+    }
+    fmt++;
+  }
+
+  *out = '\0'; 
+  return len;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);
+  int ret = vsprintf(out, fmt, args);
+  va_end(args);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
