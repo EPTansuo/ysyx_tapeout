d543d5964e0111dc95d73b2a5dd6606b71aff09f
tracer-ysyx <tracer@ysyx.org>
2024-04-05 10:03:00 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.23-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 27 Mar 2024 07:47:20 +0000 x86_64 GNU/Linux  10:03:00 up 17:15,  1 user,  load average: 2.31, 2.33, 2.11

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -79,7 +79,7 @@ void verilator_sim(int argc, char **argv)
 	VlUnpacked<unsigned char, 131072>& insts = inst_rom1->insts;
 	size_t inst_num = sizeof(img)/sizeof(uint32_t);
 	// 使用循环来复制数据
-	for (size_t i = 0; i < inst_num; ++i) {
+	for (size_t i = 0; i < inst_num * 4; ++i) {
 	insts[i] = new_insts[i];
 	//std::cout<<insts[i]<<std::endl;
 	printf("0x%02x\n", insts[i]);
