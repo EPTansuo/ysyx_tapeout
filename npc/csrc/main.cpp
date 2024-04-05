736e99e505342f4d2b41380f999657ac779967c3
tracer-ysyx <tracer@ysyx.org>
2024-04-05 10:02:39 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.23-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 27 Mar 2024 07:47:20 +0000 x86_64 GNU/Linux  10:02:39 up 17:15,  1 user,  load average: 2.43, 2.36, 2.11

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -81,7 +81,8 @@ void verilator_sim(int argc, char **argv)
 	// 使用循环来复制数据
 	for (size_t i = 0; i < inst_num; ++i) {
 	insts[i] = new_insts[i];
-	std::cout<<insts[i]<<std::endl;
+	//std::cout<<insts[i]<<std::endl;
+	printf("0x%02x\n", insts[i]);
 	}
 
 	// 释放 new_insts 数组，如果它是动态分配的
