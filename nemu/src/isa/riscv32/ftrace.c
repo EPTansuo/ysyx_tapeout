5c175883d07421450dccc94f0b8ad7c4a5caff7a
tracer-ysyx <tracer@ysyx.org>
2024-04-13 21:03:23 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 05 Apr 2024 14:16:56 +0000 x86_64 GNU/Linux  21:03:23 up 12:24,  1 user,  load average: 2.70, 2.48, 2.22

--- nemu/src/isa/riscv32/ftrace.c
+++ nemu/src/isa/riscv32/ftrace.c
@@ -202,12 +202,12 @@ void ftrace_func_call_list_print()
         while(p != NULL){
                 depth = p->depth;
                 printf("0x%lx:  ", p->addr1);
-                while(depth--) puts("  ");
+                while(depth--) printf("  ");
                 if(p->type == FUNC_CALL){
-                        puts("call ");
+                        printf("call ");
                 } 
                 else if( p-> type == FUNC_RET){
-                        puts("ret  ");
+                        printf("ret  ");
                 }
                 printf("[%s@0x%lx]\n", func_list[p->func_index].name, p->addr2);
                 p = p->prev;
