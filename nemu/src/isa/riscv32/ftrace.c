a4c1c1f5167376966ab165e49734a2409f1139f2
tracer-ysyx <tracer@ysyx.org>
2024-04-13 18:57:33 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 05 Apr 2024 14:16:56 +0000 x86_64 GNU/Linux  18:57:33 up 10:18,  1 user,  load average: 3.26, 2.03, 2.04

--- nemu/src/isa/riscv32/ftrace.c
+++ nemu/src/isa/riscv32/ftrace.c
@@ -116,8 +116,9 @@ void phase_elf(const char* _elf_file)
                 {
                         func_list[index].pc = symtab[i].st_value;
                         get_symtab_entry_name(fp, buf, strtab_hdr, &symtab[i]);
-		        func_list[index++].name = (char*)malloc(strlen(buf)+1);
+		        func_list[index].name = (char*)malloc(strlen(buf)+1);
                         strcpy(func_list[i].name, buf);
+                        index++;
                 }		
 	}       
 
