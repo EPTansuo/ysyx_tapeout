d446e8d6ee7b34038a0dd90488a16868409da6a8
tracer-ysyx <tracer@ysyx.org>
2024-04-13 09:54:51 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 05 Apr 2024 14:16:56 +0000 x86_64 GNU/Linux  09:54:51 up  1:16,  1 user,  load average: 1.31, 1.42, 1.56

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -52,10 +52,11 @@ static void out_of_bound(paddr_t addr) {
 void print_memwrite(paddr_t addr, int len, word_t data){
   word_t origin_mem = 0;
   for(int i = len-1; i >= 0; i--){
-    printf("memwrite: 0x%08x:    ", (paddr_t)addr + i);
+    printf("\nmemwrite: 0x%08x:    ", (paddr_t)addr + i);
     if (likely(in_pmem(addr))) 
       origin_mem=  pmem_read(addr, len);
-    IFDEF(CONFIG_DEVICE, origin_mem = mmio_read(addr, len));
+    else
+      IFDEF(CONFIG_DEVICE, origin_mem = mmio_read(addr, len));
     //out_of_bound(addr);
     printf("0x%02x  =>  ", (unsigned int)origin_mem);
 
@@ -80,7 +81,7 @@ word_t paddr_read(paddr_t addr, int len) {
     mem_read = pmem_read(addr, len);
 #ifdef CONFIG_MTRACE
     for(int i = len-1; i>=0; i--){
-      printf("memread: 0x%08x:    ", (paddr_t)addr + i);
+      printf("\nmemread: 0x%08x:    ", (paddr_t)addr + i);
       printf("0x%02x\n", (unsigned int)mem_read >> (i * 8) & 0xff);
     }
     return mem_read;
