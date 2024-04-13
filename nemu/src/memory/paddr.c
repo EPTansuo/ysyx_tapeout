3a73844bc4b4e5e79a4aabed902be8f0bb1187e5
tracer-ysyx <tracer@ysyx.org>
2024-04-13 09:41:44 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 05 Apr 2024 14:16:56 +0000 x86_64 GNU/Linux  09:41:44 up  1:03,  1 user,  load average: 2.08, 1.88, 1.65

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -78,7 +78,7 @@ word_t paddr_read(paddr_t addr, int len) {
   word_t mem_read = 0;
   if (likely(in_pmem(addr))) mem_read = pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, mem_read = mmio_read(addr, len));
-  out_of_bound(addr);
+  //out_of_bound(addr);
 #ifdef CONFIG_MTRACE
   for(int i = len-1; i>=0; i--){
     printf("memread: 0x%016lx:    ", (word_t)addr + i);
