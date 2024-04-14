23f58d743402163430e9ef998ee1c3fb71fe77c9
tracer-ysyx <tracer@ysyx.org>
2024-04-14 19:12:58 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 05 Apr 2024 14:16:56 +0000 x86_64 GNU/Linux  19:12:58 up 11 min,  1 user,  load average: 1.33, 1.52, 1.05

--- npc/vsrc/idu.v
+++ npc/vsrc/idu.v
@@ -101,7 +101,7 @@ always @(*)begin
                         src1 = 0;
                         src2 = 0;
                         imm = 0;
-                        if(idu_pc > `Init_Addr) begin 
+                        if(idu_pc >= `Init_Addr) begin 
                                 inst_invalid();
                         end
                 end
