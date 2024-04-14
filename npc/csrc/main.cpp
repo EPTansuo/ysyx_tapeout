f3cd8d9711c2b9015476e2493207ffffdf6ae799
tracer-ysyx <tracer@ysyx.org>
2024-04-14 18:12:12 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 05 Apr 2024 14:16:56 +0000 x86_64 GNU/Linux  18:12:12 up  8:18,  1 user,  load average: 4.23, 2.69, 2.54

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -146,7 +146,7 @@ void verilator_sim(int argc, char **argv)
 	//top->inst_rom1->insts = init_insts();
 	reset(10);
 	tfp->dump(contextp->time());
-	for (int i = 0; i < 9 && ! stop; i++)
+	for (int i = 0; i < 20 && ! stop; i++)
 	{
 		
 		single_cycle();
