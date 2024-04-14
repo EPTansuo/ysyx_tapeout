39a8e4b72032fb0c610749e09282ba61c636d14a
tracer-ysyx <tracer@ysyx.org>
2024-04-14 13:41:52 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 05 Apr 2024 14:16:56 +0000 x86_64 GNU/Linux  13:41:52 up  3:48,  1 user,  load average: 3.31, 3.71, 3.12

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -33,7 +33,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(succ)
     return true;
   
-  printf("Different ERROR!\n  pc: 0x%lx", pc);
+  printf("Different ERROR!\n  pc: 0x%lx\n", pc);
   print_iringbuf();
   return false;
 }
