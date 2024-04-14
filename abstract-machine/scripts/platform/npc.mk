db441c146c05faded17a7835aed9d07bc7b7c319
tracer-ysyx <tracer@ysyx.org>
2024-04-14 20:50:03 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 05 Apr 2024 14:16:56 +0000 x86_64 GNU/Linux  20:50:03 up  1:48,  1 user,  load average: 1.30, 2.19, 2.25

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -14,7 +14,7 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
 LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 
-NPCFLAGS += -v
+NPCFLAGS +=
 
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
