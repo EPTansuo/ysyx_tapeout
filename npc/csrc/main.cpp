0a6cd1b80d7241a81a617db2a8182c35eaf82412
tracer-ysyx <tracer@ysyx.org>
2024-04-14 18:18:52 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 05 Apr 2024 14:16:56 +0000 x86_64 GNU/Linux  18:18:52 up  8:25,  1 user,  load average: 3.60, 3.65, 3.11

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -164,8 +164,8 @@ void verilator_sim(int argc, char **argv)
 	//std::cout<< "Ebreak at pc: "<<pc->pc<<"\t Inst: ";
 	//print_inst(inst_rom1->insts, pc->pc);
 	if(!stop){
-		std::cout << YELLOW "Maximum cycle reached!" NONE<<std::endl;
-		printf(" pc: 0x" FMT_WORD_HEX_WIDTH "\n", pc->pc);
+		std::cout << YELLOW "Maximum cycle reached!" NONE;
+		printf("at pc: 0x" FMT_WORD_HEX_WIDTH "\n", pc->pc);
 	}
 	else{
 		if(gpr1->regs[10] == 0)
