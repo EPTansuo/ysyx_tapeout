fbe6599c999f3ad8759ce0888125d07fcb69b770
tracer-ysyx <tracer@ysyx.org>
2024-04-14 17:34:45 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 05 Apr 2024 14:16:56 +0000 x86_64 GNU/Linux  17:34:45 up  7:41,  1 user,  load average: 2.67, 2.95, 2.86

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -77,6 +77,8 @@ void init_insts(VlUnpacked<unsigned char, 131072>& insts){
 void print_inst(const VlUnpacked<unsigned char, 131072>& insts, size_t pc)
 {
 	size_t index = pc - 0x80000000;
+	std::cout << std::hex << std::setw(8) << std::setfill('0')
+		<< pc << " : ";
 	std::cout << std::hex << std::setw(2) << std::setfill('0')
 		<< static_cast<int>(insts[index+3]) << " "
 		<< std::setw(2) << static_cast<int>(insts[index+2]) << " "
