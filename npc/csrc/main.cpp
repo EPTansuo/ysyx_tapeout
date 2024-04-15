59c820fc582741e195da79217f4421f3cba78083
tracer-ysyx <tracer@ysyx.org>
2024-04-15 14:43:32 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 05 Apr 2024 14:16:56 +0000 x86_64 GNU/Linux  14:43:32 up 14 min,  1 user,  load average: 2.57, 2.25, 1.56

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -60,9 +60,11 @@ Disasm *disasm;
 void init_disasm(const char* _img_file){
 	char img_file_path[200];
 	char line[100];
+	char file_path[100] = "./build/";
 	uint16_t lines = 0;
-	strcpy(img_file_path,"./build/");
-	strcat(img_file_path,_img_file);
+	strcpy(img_file_path,_img_file);
+	strcat(file_path,img_file_path);
+	strcpy(img_file_path,file_path);
 	char *base_file_name = basename(img_file_path);
 	strcat(base_file_name,".disasm");
 	FILE* fp = fopen(base_file_name,"r");
