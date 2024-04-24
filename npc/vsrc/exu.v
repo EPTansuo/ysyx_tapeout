f5ad291352c1eaa3a8235df4606536419bcbae67
tracer-ysyx <tracer@ysyx.org>
2024-04-24 18:38:19 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.27-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 13 Apr 2024 11:50:59 +0000 x86_64 GNU/Linux  18:38:19 up  5:33,  1 user,  load average: 2.04, 1.89, 2.04

--- npc/vsrc/exu.v
+++ npc/vsrc/exu.v
@@ -67,14 +67,14 @@ wire [`WordBus] add_src1_imm;
 assign add_src1_imm = src1+imm;
 
 `ifndef STA
-assign exu_invalid_inst = rst == `RstEnable ? 1'b0 :
-                          inst_type == `Inst_addi ? 1'b0 :
-                          inst_type == `Inst_auipc ? 1'b0 :
-                          inst_type == `Inst_lui ? 1'b0 :
-                          inst_type == `Inst_jal ? 1'b0 :
-                          inst_type == `Inst_jalr ? 1'b0 :
-                          inst_type == `Inst_sw ? 1'b0 :
-                          1'b1;
+assign exu_invalid_inst = rst == `RstEnable ? 0 : 
+                          inst_type == `Inst_addi ? 0 : 
+                          inst_type == `Inst_auipc ? 0 : 
+                          inst_type == `Inst_lui ? 0 : 
+                          inst_type == `Inst_jal ? 0 : 
+                          inst_type == `Inst_jalr ? 0 : 
+                          inst_type == `Inst_sw ? 0 : 
+                          1;
 `endif
 
 
