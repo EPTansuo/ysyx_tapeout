71137e9f24c9c2d1d47b1a8387853f5710a6dd82
tracer-ysyx <tracer@ysyx.org>
2024-04-25 08:29:48 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.28-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 17 Apr 2024 10:11:09 +0000 x86_64 GNU/Linux  08:29:48 up 2 min,  1 user,  load average: 4.63, 1.76, 0.64

--- npc/csrc/cpu/cpu-exec.cpp
+++ npc/csrc/cpu/cpu-exec.cpp
@@ -28,8 +28,8 @@ void disassemble(char* logbuf, size_t logbuf_size, word_t pc);
 
 static void trace_and_difftest(){
   //printf("pc=0x%x, dnpc=0x%x\n",top->cpu->pc1->pc, top->cpu->pc1->pc + (top->cpu->pc1->pc_offset_en?top->cpu->pc1->pc_offset:0));
-  IFDEF(CONFIG_DIFFTEST, difftest_step(top->cpu->pc1->pc, top->cpu->pc1->pc + top->cpu->pc1->pc_offset));
-  
+  //IFDEF(CONFIG_DIFFTEST, difftest_step(top->cpu->pc1->pc, top->cpu->pc1->pc + top->cpu->pc1->pc_offset));
+  IFDEF(CONFIG_DIFFTEST, difftest_step(0,0));
   scan_watchpoint();
 }
 
