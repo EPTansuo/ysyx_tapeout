d43fb6096446c226be3d5703b422836261277b32
tracer-ysyx <tracer@ysyx.org>
2024-04-25 15:06:37 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.28-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 17 Apr 2024 10:11:09 +0000 x86_64 GNU/Linux  15:06:37 up  1:31,  1 user,  load average: 1.91, 2.14, 2.22

--- npc/vsrc/exu.v
+++ npc/vsrc/exu.v
@@ -69,265 +69,44 @@ assign add_src1_imm = src1+imm;
 `ifndef STA
 assign exu_invalid_inst = rst == `RstEnable ? 0 : 
                           inst_type == `Inst_ebreak ? 0 : 
-                          inst_type == `Inst_addi ? 0 : 
-                          inst_type == `Inst_auipc ? 0 : 
-                          inst_type == `Inst_lui ? 0 : 
-                          inst_type == `Inst_jal ? 0 : 
-                          inst_type == `Inst_jalr ? 0 : 
-                          inst_type == `Inst_beq ? 0 : 
-                          inst_type == `Inst_bne ? 0 : 
-                          inst_type == `Inst_blt ? 0 : 
-                          inst_type == `Inst_bge ? 0 : 
-                          inst_type == `Inst_or ? 0 : 
-                          inst_type == `Inst_xor ? 0 : 
-                          inst_type == `Inst_sltu ? 0 : 
-                          inst_type == `Inst_sltiu ? 0 : 
-                          inst_type == `Inst_sub ? 0 : 
-                          inst_type == `Inst_add ? 0 : 
-                          inst_type == `Inst_lw ? 0 : 
-                          inst_type == `Inst_sw ? 0 : 
                           1;
 `endif
 
 
-assign gpr_we = inst_type == `Inst_addi ? `Enable : 
-                inst_type == `Inst_auipc ? `Enable : 
-                inst_type == `Inst_lui ? `Enable : 
-                inst_type == `Inst_jal ? `Enable : 
-                inst_type == `Inst_jalr ? `Enable : 
-                inst_type == `Inst_beq ? `Disable : 
-                inst_type == `Inst_bne ? `Disable : 
-                inst_type == `Inst_blt ? `Disable : 
-                inst_type == `Inst_bge ? `Disable : 
-                inst_type == `Inst_or ? `Enable : 
-                inst_type == `Inst_xor ? `Enable : 
-                inst_type == `Inst_sltu ? `Enable : 
-                inst_type == `Inst_sltiu ? `Enable : 
-                inst_type == `Inst_sub ? `Enable : 
-                inst_type == `Inst_add ? `Enable : 
-                inst_type == `Inst_lw ? `Enable : 
-                inst_type == `Inst_sw ? `Disable : 
-                `Disable;
+assign gpr_we = `Disable;
 
 
-assign gpr_w_addr = inst_type == `Inst_addi ? rd : 
-                    inst_type == `Inst_auipc ? rd : 
-                    inst_type == `Inst_lui ? rd : 
-                    inst_type == `Inst_jal ? rd : 
-                    inst_type == `Inst_jalr ? rd : 
-                    inst_type == `Inst_beq ? 0 : 
-                    inst_type == `Inst_bne ? 0 : 
-                    inst_type == `Inst_blt ? 0 : 
-                    inst_type == `Inst_bge ? 0 : 
-                    inst_type == `Inst_or ? rd : 
-                    inst_type == `Inst_xor ? rd : 
-                    inst_type == `Inst_sltu ? rd : 
-                    inst_type == `Inst_sltiu ? rd : 
-                    inst_type == `Inst_sub ? rd : 
-                    inst_type == `Inst_add ? rd : 
-                    inst_type == `Inst_lw ? rd : 
-                    inst_type == `Inst_sw ? 0 : 
-                    0;
+assign gpr_w_addr = 0;
 
 
-assign gpr_w_data = inst_type == `Inst_addi ? add_src1_imm : 
-                    inst_type == `Inst_auipc ? add_src1_imm : 
-                    inst_type == `Inst_lui ? imm : 
-                    inst_type == `Inst_jal ? idu_pc+4 : 
-                    inst_type == `Inst_jalr ? idu_pc+4 : 
-                    inst_type == `Inst_beq ? 0 : 
-                    inst_type == `Inst_bne ? 0 : 
-                    inst_type == `Inst_blt ? 0 : 
-                    inst_type == `Inst_bge ? 0 : 
-                    inst_type == `Inst_or ? src1|src2 : 
-                    inst_type == `Inst_xor ? src1^src2 : 
-                    inst_type == `Inst_sltu ? src1<src2?1:0 : 
-                    inst_type == `Inst_sltiu ? src1<imm?1:0 : 
-                    inst_type == `Inst_sub ? src1-src2 : 
-                    inst_type == `Inst_add ? src1+src2 : 
-                    inst_type == `Inst_lw ? mem_r_data : 
-                    inst_type == `Inst_sw ? 0 : 
-                    0;
+assign gpr_w_data = 0;
 
 
-assign pc_offset_en = inst_type == `Inst_addi ? `Disable : 
-                      inst_type == `Inst_auipc ? `Disable : 
-                      inst_type == `Inst_lui ? `Disable : 
-                      inst_type == `Inst_jal ? `Enable : 
-                      inst_type == `Inst_jalr ? `Enable : 
-                      inst_type == `Inst_beq ? `Enable : 
-                      inst_type == `Inst_bne ? `Enable : 
-                      inst_type == `Inst_blt ? `Enable : 
-                      inst_type == `Inst_bge ? `Enable : 
-                      inst_type == `Inst_or ? `Disable : 
-                      inst_type == `Inst_xor ? `Disable : 
-                      inst_type == `Inst_sltu ? `Disable : 
-                      inst_type == `Inst_sltiu ? `Disable : 
-                      inst_type == `Inst_sub ? `Disable : 
-                      inst_type == `Inst_add ? `Disable : 
-                      inst_type == `Inst_lw ? `Disable : 
-                      inst_type == `Inst_sw ? `Disable : 
-                      `Disable;
+assign pc_offset_en = `Disable;
 
 
-assign pc_offset = inst_type == `Inst_addi ? 4 : 
-                   inst_type == `Inst_auipc ? 4 : 
-                   inst_type == `Inst_lui ? 4 : 
-                   inst_type == `Inst_jal ? imm : 
-                   inst_type == `Inst_jalr ? add_src1_imm - idu_pc : 
-                   inst_type == `Inst_beq ? src1==src2?imm:4 : 
-                   inst_type == `Inst_bne ? src1!=src2?imm:4 : 
-                   inst_type == `Inst_blt ? $signed(src1)<$signed(src2)? imm : 4 : 
-                   inst_type == `Inst_bge ? $signed(src1)>$signed(src2)? imm : 4 : 
-                   inst_type == `Inst_or ? 4 : 
-                   inst_type == `Inst_xor ? 4 : 
-                   inst_type == `Inst_sltu ? 4 : 
-                   inst_type == `Inst_sltiu ? 4 : 
-                   inst_type == `Inst_sub ? 4 : 
-                   inst_type == `Inst_add ? 4 : 
-                   inst_type == `Inst_lw ? 4 : 
-                   inst_type == `Inst_sw ? 4 : 
-                   4;
+assign pc_offset = 4;
 
 
-assign mem_re = inst_type == `Inst_addi ? `Disable : 
-                inst_type == `Inst_auipc ? `Disable : 
-                inst_type == `Inst_lui ? `Disable : 
-                inst_type == `Inst_jal ? `Disable : 
-                inst_type == `Inst_jalr ? `Disable : 
-                inst_type == `Inst_beq ? `Disable : 
-                inst_type == `Inst_bne ? `Disable : 
-                inst_type == `Inst_blt ? `Disable : 
-                inst_type == `Inst_bge ? `Disable : 
-                inst_type == `Inst_or ? `Disable : 
-                inst_type == `Inst_xor ? `Disable : 
-                inst_type == `Inst_sltu ? `Disable : 
-                inst_type == `Inst_sltiu ? `Disable : 
-                inst_type == `Inst_sub ? `Disable : 
-                inst_type == `Inst_add ? `Disable : 
-                inst_type == `Inst_lw ? `Enable : 
-                inst_type == `Inst_sw ? `Disable : 
-                `Disable;
+assign mem_re = `Disable;
 
 
-assign mem_r_addr = inst_type == `Inst_addi ? 0 : 
-                    inst_type == `Inst_auipc ? 0 : 
-                    inst_type == `Inst_lui ? 0 : 
-                    inst_type == `Inst_jal ? 0 : 
-                    inst_type == `Inst_jalr ? 0 : 
-                    inst_type == `Inst_beq ? 0 : 
-                    inst_type == `Inst_bne ? 0 : 
-                    inst_type == `Inst_blt ? 0 : 
-                    inst_type == `Inst_bge ? 0 : 
-                    inst_type == `Inst_or ? 0 : 
-                    inst_type == `Inst_xor ? 0 : 
-                    inst_type == `Inst_sltu ? 0 : 
-                    inst_type == `Inst_sltiu ? 0 : 
-                    inst_type == `Inst_sub ? 0 : 
-                    inst_type == `Inst_add ? 0 : 
-                    inst_type == `Inst_lw ? add_src1_imm : 
-                    inst_type == `Inst_sw ? 0 : 
-                    0;
+assign mem_r_addr = 0;
 
 
-assign mem_r_mask = inst_type == `Inst_addi ? 0 : 
-                    inst_type == `Inst_auipc ? 0 : 
-                    inst_type == `Inst_lui ? 0 : 
-                    inst_type == `Inst_jal ? 0 : 
-                    inst_type == `Inst_jalr ? 0 : 
-                    inst_type == `Inst_beq ? 0 : 
-                    inst_type == `Inst_bne ? 0 : 
-                    inst_type == `Inst_blt ? 0 : 
-                    inst_type == `Inst_bge ? 0 : 
-                    inst_type == `Inst_or ? 0 : 
-                    inst_type == `Inst_xor ? 0 : 
-                    inst_type == `Inst_sltu ? 0 : 
-                    inst_type == `Inst_sltiu ? 0 : 
-                    inst_type == `Inst_sub ? 0 : 
-                    inst_type == `Inst_add ? 0 : 
-                    inst_type == `Inst_lw ? 8'b00001111 : 
-                    inst_type == `Inst_sw ? 0 : 
-                    0;
+assign mem_r_mask = 0;
 
 
-assign mem_we = inst_type == `Inst_addi ? `Disable : 
-                inst_type == `Inst_auipc ? `Disable : 
-                inst_type == `Inst_lui ? `Disable : 
-                inst_type == `Inst_jal ? `Disable : 
-                inst_type == `Inst_jalr ? `Disable : 
-                inst_type == `Inst_beq ? `Disable : 
-                inst_type == `Inst_bne ? `Disable : 
-                inst_type == `Inst_blt ? `Disable : 
-                inst_type == `Inst_bge ? `Disable : 
-                inst_type == `Inst_or ? `Disable : 
-                inst_type == `Inst_xor ? `Disable : 
-                inst_type == `Inst_sltu ? `Disable : 
-                inst_type == `Inst_sltiu ? `Disable : 
-                inst_type == `Inst_sub ? `Disable : 
-                inst_type == `Inst_add ? `Disable : 
-                inst_type == `Inst_lw ? `Disable : 
-                inst_type == `Inst_sw ? `Enable : 
-                `Disable;
+assign mem_we = `Disable;
 
 
-assign mem_w_addr = inst_type == `Inst_addi ? 0 : 
-                    inst_type == `Inst_auipc ? 0 : 
-                    inst_type == `Inst_lui ? 0 : 
-                    inst_type == `Inst_jal ? 0 : 
-                    inst_type == `Inst_jalr ? 0 : 
-                    inst_type == `Inst_beq ? 0 : 
-                    inst_type == `Inst_bne ? 0 : 
-                    inst_type == `Inst_blt ? 0 : 
-                    inst_type == `Inst_bge ? 0 : 
-                    inst_type == `Inst_or ? 0 : 
-                    inst_type == `Inst_xor ? 0 : 
-                    inst_type == `Inst_sltu ? 0 : 
-                    inst_type == `Inst_sltiu ? 0 : 
-                    inst_type == `Inst_sub ? 0 : 
-                    inst_type == `Inst_add ? 0 : 
-                    inst_type == `Inst_lw ? 0 : 
-                    inst_type == `Inst_sw ? add_src1_imm : 
-                    0;
+assign mem_w_addr = 0;
 
 
-assign mem_w_data = inst_type == `Inst_addi ? 0 : 
-                    inst_type == `Inst_auipc ? 0 : 
-                    inst_type == `Inst_lui ? 0 : 
-                    inst_type == `Inst_jal ? 0 : 
-                    inst_type == `Inst_jalr ? 0 : 
-                    inst_type == `Inst_beq ? 0 : 
-                    inst_type == `Inst_bne ? 0 : 
-                    inst_type == `Inst_blt ? 0 : 
-                    inst_type == `Inst_bge ? 0 : 
-                    inst_type == `Inst_or ? 0 : 
-                    inst_type == `Inst_xor ? 0 : 
-                    inst_type == `Inst_sltu ? 0 : 
-                    inst_type == `Inst_sltiu ? 0 : 
-                    inst_type == `Inst_sub ? 0 : 
-                    inst_type == `Inst_add ? 0 : 
-                    inst_type == `Inst_lw ? 0 : 
-                    inst_type == `Inst_sw ? src2 : 
-                    0;
+assign mem_w_data = 0;
 
 
-assign mem_w_mask = inst_type == `Inst_addi ? 0 : 
-                    inst_type == `Inst_auipc ? 0 : 
-                    inst_type == `Inst_lui ? 0 : 
-                    inst_type == `Inst_jal ? 0 : 
-                    inst_type == `Inst_jalr ? 0 : 
-                    inst_type == `Inst_beq ? 0 : 
-                    inst_type == `Inst_bne ? 0 : 
-                    inst_type == `Inst_blt ? 0 : 
-                    inst_type == `Inst_bge ? 0 : 
-                    inst_type == `Inst_or ? 0 : 
-                    inst_type == `Inst_xor ? 0 : 
-                    inst_type == `Inst_sltu ? 0 : 
-                    inst_type == `Inst_sltiu ? 0 : 
-                    inst_type == `Inst_sub ? 0 : 
-                    inst_type == `Inst_add ? 0 : 
-                    inst_type == `Inst_lw ? 0 : 
-                    inst_type == `Inst_sw ? 8'b00001111 : 
-                    0;
+assign mem_w_mask = 0;
 
 
 
