75b39da44902912f67b4a4ce7712e06d6c68eaef
tracer-ysyx <tracer@ysyx.org>
2024-04-25 18:48:54 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.28-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 17 Apr 2024 10:11:09 +0000 x86_64 GNU/Linux  18:48:54 up  5:14,  1 user,  load average: 0.48, 0.63, 0.63

--- npc/vsrc/exu.v
+++ npc/vsrc/exu.v
@@ -72,6 +72,7 @@ assign exu_invalid_inst = rst == `RstEnable ? 0 :
                           inst_type == `Inst_addi ? 0 : 
                           inst_type == `Inst_auipc ? 0 : 
                           inst_type == `Inst_jal ? 0 : 
+                          inst_type == `Inst_sw ? 0 : 
                           1;
 `endif
 
@@ -79,72 +80,84 @@ assign exu_invalid_inst = rst == `RstEnable ? 0 :
 assign gpr_we = inst_type == `Inst_addi ? `Enable : 
                 inst_type == `Inst_auipc ? `Enable : 
                 inst_type == `Inst_jal ? `Enable : 
+                inst_type == `Inst_sw ? `Disable : 
                 `Disable;
 
 
 assign gpr_w_addr = inst_type == `Inst_addi ? rd : 
                     inst_type == `Inst_auipc ? rd : 
                     inst_type == `Inst_jal ? rd : 
+                    inst_type == `Inst_sw ? 0 : 
                     0;
 
 
 assign gpr_w_data = inst_type == `Inst_addi ? add_src1_imm : 
                     inst_type == `Inst_auipc ? idu_pc+imm : 
                     inst_type == `Inst_jal ? idu_pc+4 : 
+                    inst_type == `Inst_sw ? 0 : 
                     0;
 
 
 assign pc_offset_en = inst_type == `Inst_addi ? `Disable : 
                       inst_type == `Inst_auipc ? `Disable : 
                       inst_type == `Inst_jal ? `Enable : 
+                      inst_type == `Inst_sw ? `Disable : 
                       `Disable;
 
 
 assign pc_offset = inst_type == `Inst_addi ? 4 : 
                    inst_type == `Inst_auipc ? 4 : 
                    inst_type == `Inst_jal ? imm : 
+                   inst_type == `Inst_sw ? 4 : 
                    4;
 
 
 assign mem_re = inst_type == `Inst_addi ? `Disable : 
                 inst_type == `Inst_auipc ? `Disable : 
                 inst_type == `Inst_jal ? `Disable : 
+                inst_type == `Inst_sw ? `Disable : 
                 `Disable;
 
 
 assign mem_r_addr = inst_type == `Inst_addi ? 0 : 
                     inst_type == `Inst_auipc ? 0 : 
                     inst_type == `Inst_jal ? 0 : 
+                    inst_type == `Inst_sw ? 0 : 
                     0;
 
 
 assign mem_r_mask = inst_type == `Inst_addi ? 0 : 
                     inst_type == `Inst_auipc ? 0 : 
                     inst_type == `Inst_jal ? 0 : 
+                    inst_type == `Inst_sw ? 0 : 
                     0;
 
 
 assign mem_we = inst_type == `Inst_addi ? `Disable : 
                 inst_type == `Inst_auipc ? `Disable : 
                 inst_type == `Inst_jal ? `Disable : 
+                inst_type == `Inst_sw ? `Enable : 
                 `Disable;
 
 
 assign mem_w_addr = inst_type == `Inst_addi ? 0 : 
                     inst_type == `Inst_auipc ? 0 : 
                     inst_type == `Inst_jal ? 0 : 
+                    inst_type == `Inst_sw ? add_src1_imm : 
                     0;
 
 
 assign mem_w_data = inst_type == `Inst_addi ? 0 : 
                     inst_type == `Inst_auipc ? 0 : 
                     inst_type == `Inst_jal ? 0 : 
+                    inst_type == `Inst_sw ? src2 : 
                     0;
 
 
 assign mem_w_mask = inst_type == `Inst_addi ? 0 : 
                     inst_type == `Inst_auipc ? 0 : 
                     inst_type == `Inst_jal ? 0 : 
+                    inst_type == `Inst_sw ? 8'b00001111 : 
                     0;
 
 
