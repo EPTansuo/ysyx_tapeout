1183980de48a2b7aec2ddba43a47cf4cd30df22a
tracer-ysyx <tracer@ysyx.org>
2024-04-25 11:21:57 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.28-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 17 Apr 2024 10:11:09 +0000 x86_64 GNU/Linux  11:21:57 up 57 min,  1 user,  load average: 1.53, 1.32, 1.17

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -81,9 +81,6 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
-#ifdef CONFIG_TARGET_SHARE
-  printf("nemu: pc = " FMT_WORD "\t" FMT_WORD "\n",  _this->pc, _this->isa.inst.val);
-#endif 
 
   scan_watchpoint();
 }
