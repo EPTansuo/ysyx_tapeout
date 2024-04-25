8169cba959c9bc9c0e833a2f57d1fbfc51f9e955
tracer-ysyx <tracer@ysyx.org>
2024-04-25 11:27:45 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.28-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 17 Apr 2024 10:11:09 +0000 x86_64 GNU/Linux  11:27:45 up  1:03,  1 user,  load average: 1.49, 1.37, 1.23

--- npc/csrc/isa/riscv32/difftest/rv_dut.c
+++ npc/csrc/isa/riscv32/difftest/rv_dut.c
@@ -32,6 +32,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(!first){
     first = true;
     succ = memcmp(&cpu_state_buf, &npc_cpu, DIFFTEST_REG_SIZE) == 0;
+    printf("npc:nemu:pc==0x%x\n",ref_r->pc);
     if(!succ){
       printf("\e[1;31m Difftest ERROR!\e[0m\n  pc: 0x" FMT_WORD_HEX "\n", pc);
       return false;
