233974c4d79c87a77d7569211b64e9059c279b81
tracer-ysyx <tracer@ysyx.org>
2024-04-25 20:53:20 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.28-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 17 Apr 2024 10:11:09 +0000 x86_64 GNU/Linux  20:53:20 up  7:18,  1 user,  load average: 0.83, 0.64, 0.79

--- npc/vsrc/exu.v
+++ npc/vsrc/exu.v
@@ -216,9 +216,9 @@ assign gpr_w_data = inst_type == `Inst_addi ? add_src1_imm :
                     inst_type == `Inst_sll ? src1 << src2[4:0] : 
                     inst_type == `Inst_srli ? src1 >> shamt_long : 
                     inst_type == `Inst_slli ? src1 << shamt_long : 
-                    inst_type == `Inst_lbu ? {{(`WordWidth-8){1'b0}}, mem_r_data[7:0]} : 
-                    inst_type == `Inst_lhu ? {{(`WordWidth-16){1'b0}}, mem_r_data[15:0]} : 
-                    inst_type == `Inst_lh ? {{(`WordWidth-16){mem_r_data[15]}}, mem_r_data[15:0]} : 
+                    inst_type == `Inst_lbu ? {{(`WordWidth-8){1'b0}}, mem_r_data[`WordWidth-1:`WordWidth-8]} : 
+                    inst_type == `Inst_lhu ? {{(`WordWidth-16){1'b0}}, mem_r_data[`WordWidth-1:`WordWidth-16]} : 
+                    inst_type == `Inst_lh ? {{(`WordWidth-16){mem_r_data[`WordWidth-1]}}, mem_r_data[`WordWidth-1:`WordWidth-16]} : 
                     inst_type == `Inst_lw ? mem_r_data : 
                     inst_type == `Inst_sb ? 0 : 
                     inst_type == `Inst_sh ? 0 : 
