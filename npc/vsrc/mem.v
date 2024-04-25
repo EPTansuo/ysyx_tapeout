c79701c9f8b03496b023660f4e22a96893735917
tracer-ysyx <tracer@ysyx.org>
2024-04-25 12:19:59 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.28-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 17 Apr 2024 10:11:09 +0000 x86_64 GNU/Linux  12:19:59 up  1:55,  1 user,  load average: 1.39, 0.96, 0.90

--- npc/vsrc/mem.v
+++ npc/vsrc/mem.v
@@ -25,31 +25,44 @@ import "DPI-C" function int pmem_read(input int raddr);
 import "DPI-C" function void pmem_write(input int waddr, input int wdata, input byte wmask);
 
 
+
 always @(*) begin
-        if (rst == `RstEnable) begin
-                r_data = 0;
+        if (rst == `RstDisable) begin // 有读写请求时
+          r_data = pmem_read(r_addr);
+          if (we) begin // 有写请求时
+            pmem_write(w_addr, w_data, w_mask);
+          end
         end
         else begin
-                if(re == `Enable) begin
-                        r_data = pmem_read(r_addr);
-                end
-                else begin
-                        r_data = 0;
-                end
+          r_data = 0;
         end
-end
+      end
+
+// always @(*) begin
+//         if (rst == `RstEnable) begin
+//                 r_data = 0;
+//         end
+//         else begin
+//                 if(re == `Enable) begin
+//                         r_data = pmem_read(r_addr);
+//                 end
+//                 else begin
+//                         r_data = 0;
+//                 end
+//         end
+// end
 
 
-always @(posedge clk) begin
-        if (rst == `RstEnable) begin
-                // Do nothing
-        end
-        else begin
-                if(we == `Enable) begin
-                        pmem_write(w_addr, w_data, w_mask);
-                end
-        end
-end
+// always @(posedge clk) begin
+//         if (rst == `RstEnable) begin
+//                 // Do nothing
+//         end
+//         else begin
+//                 if(we == `Enable) begin
+//                         pmem_write(w_addr, w_data, w_mask);
+//                 end
+//         end
+// end
 
 
 endmodule
