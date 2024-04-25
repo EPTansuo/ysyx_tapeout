7f89cfa02d0e0f63cd2555a28453b439a6595d15
tracer-ysyx <tracer@ysyx.org>
2024-04-25 11:21:09 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.28-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 17 Apr 2024 10:11:09 +0000 x86_64 GNU/Linux  11:21:09 up 56 min,  1 user,  load average: 1.14, 1.21, 1.13

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -178,6 +178,6 @@ static int decode_exec(Decode *s) {
 
 int isa_exec_once(Decode *s) {
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
-  printf("nemu: s->isa.inst.val: 0x%08x\n",s->isa.inst.val);
+  //printf("nemu: s->isa.inst.val: 0x%08x\n",s->isa.inst.val);
   return decode_exec(s);
 }
