7637a2bba6a3f6c0b30370eff34289b8dad59a93
tracer-ysyx <tracer@ysyx.org>
2024-04-25 11:54:59 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.28-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 17 Apr 2024 10:11:09 +0000 x86_64 GNU/Linux  11:54:59 up  1:30,  1 user,  load average: 1.43, 1.38, 1.26

--- npc/csrc/isa/riscv32/difftest/rv_dut.c
+++ npc/csrc/isa/riscv32/difftest/rv_dut.c
@@ -30,7 +30,6 @@ extern const char *regs[];
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   bool succ = true;
   if(!first){
-    //succ = memcmp(&cpu_state_buf, &npc_cpu, DIFFTEST_REG_SIZE) == 0;
     //if(ref_r->pc != npc_cpu.pc){
     if(false){
       succ = false;
@@ -55,7 +54,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     //     }
     //     printf("$pc = 0x" FMT_WORD_HEX_WIDTH "\n", top->cpu->pc1->pc);
     if(!succ){
-      printf("\e[1;31m Difftest ERROR!\e[0m\n  pc: 0x" FMT_WORD_HEX "\n", pc);
+      printf("\e[1;31m Difftest ERROR!\e[0m\n ");
       printf("DO NOT SEE CURRENT INSTRATION, SEE PREVIOUS ONE!\n");
       return false;
     }
