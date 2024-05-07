c37703a9931a7131825922f8df8f0194926798df
tracer-ysyx <tracer@ysyx.org>
2024-05-07 15:30:58 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.28-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 17 Apr 2024 10:11:09 +0000 x86_64 GNU/Linux  15:30:58 up  3:20,  1 user,  load average: 1.29, 2.15, 1.70

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -6,6 +6,13 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  uint32_t scr_size = inl(VGACTL_ADDR);
+  uint32_t h = scr_size & 0xffff;
+  uint32_t w = scr_size >> 16;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (size_t i = 0; i < w * h; i ++)
+    fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
