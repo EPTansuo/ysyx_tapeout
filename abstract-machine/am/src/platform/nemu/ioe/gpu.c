e8c0681fe751fb56ea504bde1cd597ab1ab16b77
tracer-ysyx <tracer@ysyx.org>
2024-05-07 15:25:06 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.28-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 17 Apr 2024 10:11:09 +0000 x86_64 GNU/Linux  15:25:06 up  3:14,  1 user,  load average: 2.76, 1.72, 1.29

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -11,16 +11,16 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
  //see func: init_vga in vga.c in nemu
   uint32_t scr_size = inl(VGACTL_ADDR);
-  uint16_t h = scr_size;
-  uint16_t w = scr_size >> 16;
+  uint32_t h = scr_size & 0xffff;
+  uint32_t w = scr_size >> 16;
   //printf("%s: h: %d, w: %d \n",__func__, h , w);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = w, .height = h,
     .vmemsz = 0
   };
-  w = io_read(AM_GPU_CONFIG).width ;
-  h = io_read(AM_GPU_CONFIG).height ;
+  //w = io_read(AM_GPU_CONFIG).width ;
+  //h = io_read(AM_GPU_CONFIG).height ;
   printf("%s: h: %d, w: %d \n",__func__, h , w);
 }
 
