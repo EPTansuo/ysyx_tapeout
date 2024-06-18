1442dfa010a24dc6e4e9fcff813461f60bf64360
tracer-ysyx <tracer@ysyx.org>
2024-06-18 10:49:07 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.32-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 25 May 2024 20:20:51 +0000 x86_64 GNU/Linux  10:49:07 up 32 min,  1 user,  load average: 2.27, 1.62, 1.58

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -44,6 +44,11 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
          (cpu.csr.mtvec == ref_r->csr.mtvec);
   if(!succ){
      printf("\e[1;31mCSR DIFFTESET ERROR!\e[0m\n");
+     printf("ref csr info:");
+     printf("mstatus: 0x%x\n", ref_r->csr.mstatus);
+      printf("mcause: 0x%x\n", ref_r->csr.mcause);
+      printf("mepc: 0x%x\n", ref_r->csr.mepc);
+      printf("mtvec: 0x%x\n", ref_r->csr.mtvec);
      goto print_error_info;
   }
 
@@ -54,6 +59,7 @@ print_error_info:
   printf("\e[1;31mDifftest ERROR!\e[0m\n  pc: 0x"FMT_WORD_HEX"\n", pc);
 
 isa_csr_display();
+
 
 #ifdef CONFIG_ITRACE
   print_iringbuf();
