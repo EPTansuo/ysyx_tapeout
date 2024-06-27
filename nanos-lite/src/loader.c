bc68b7f45e4e3ffef1005a385a3d8db3e4e5fe1e
tracer-ysyx <tracer@ysyx.org>
2024-06-27 12:57:21 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.32-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 25 May 2024 20:20:51 +0000 x86_64 GNU/Linux  12:57:21 up 1 day,  1:13,  1 user,  load average: 0.87, 0.98, 1.36

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -19,6 +19,18 @@ size_t get_ramdisk_size();
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_X86__)
+# define EXPECT_TYPE EM_X86_64  // see /usr/include/elf.h to get the right type
+#elif defined(__ISA_RISCV32__) || defined(__ISA_RISCV64__)
+# define EXPECT_TYPE EM_RISCV
+#else
+# error Unsupported ISA
+#endif
+
+
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
   Elf_Ehdr elf;
