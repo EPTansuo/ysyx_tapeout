2e76b4735999d30ec4fdbaae858f2ae00870f7a9
tracer-ysyx <tracer@ysyx.org>
2024-06-27 12:40:26 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.32-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 25 May 2024 20:20:51 +0000 x86_64 GNU/Linux  12:40:26 up 1 day, 57 min,  1 user,  load average: 1.04, 1.17, 1.94

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD : Log("yield succeed!"); c->mepc += 4; break;
+    case EVENT_YIELD : printf("Yield Event!\n"); c->mepc += 4; break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
