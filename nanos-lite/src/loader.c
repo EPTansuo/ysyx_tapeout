8528278d501d6a9d2845f1c502a9ac55454029f9
tracer-ysyx <tracer@ysyx.org>
2024-06-27 12:46:58 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.32-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 25 May 2024 20:20:51 +0000 x86_64 GNU/Linux  12:46:58 up 1 day,  1:03,  1 user,  load average: 1.52, 1.51, 1.83

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,16 @@
 #include <proc.h>
 #include <elf.h>
 
+// 从ramdisk中`offset`偏移处的`len`字节读入到`buf`中
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
+// 把`buf`中的`len`字节写入到ramdisk中`offset`偏移处
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
+// 返回ramdisk的大小, 单位为字节
+size_t get_ramdisk_size();
+
+
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
@@ -11,7 +21,20 @@
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
-  
+  Elf_Ehdr elf;
+  ramdisk_read(&elf, 0, sizeof(elf));
+  assert(*(uint32_t *)(&elf)->e_ident == 0x464c457f);
+ 
+  Elf_Phdr ph[elf.e_phnum];
+  ramdisk_read(ph, elf.e_phoff, sizeof(Elf_Phdr)*elf.e_phnum);
+  for (int i = 0; i < elf.e_phnum; i++) {
+    if (ph[i].p_type == PT_LOAD) {
+      ramdisk_read((void *)ph[i].p_vaddr, ph[i].p_offset, ph[i].p_memsz);
+      memset((void *)(ph[i].p_vaddr + ph[i].p_filesz), 0, ph[i].p_memsz - ph[i].p_filesz);
+    }
+  }
+  return elf.e_entry;
+
   return 0;
 }
 
