94cda5ed85427a824aa13d85492b1e6f07999bcd
tracer-ysyx <tracer@ysyx.org>
2024-07-02 17:31:31 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.32-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 25 May 2024 20:20:51 +0000 x86_64 GNU/Linux  17:31:31 up  6:26,  1 user,  load average: 4.41, 2.43, 1.97

--- npc/csrc/utils/disasm.c
+++ npc/csrc/utils/disasm.c
@@ -49,7 +49,7 @@ std::string replace_regs_name(const std::string& code) {
 void init_disasm(){
 	char img_file_path[200];
 	char line[100];
-	char asm_code_buf[100];
+	//char asm_code_buf[100];
 	char img_full_name[50];
 	uint16_t lines = 0;
 	strcpy(img_full_name, img_file);
@@ -74,7 +74,7 @@ void init_disasm(){
 	while (fgets(line, sizeof(line), fp) != NULL) {
 		if (sscanf(line, "%x %99[^\n]", &disasm[i].pc, &(disasm[i].str[0])) == 2) {
 		//if (sscanf(line, "%x %99[^\n]", &disasm[i].pc, asm_code_buf) == 2) {
-		printf("Address: 0x%X, Instruction: %s\n", disasm[i].pc, disasm[i].str);
+		//printf("Address: 0x%X, Instruction: %s\n", disasm[i].pc, disasm[i].str);
 			//strcpy(&(disasm[i].str[0]),replace_regs_name(asm_code_buf).c_str());
 		} else {
 		fprintf(stderr, "Failed to parse line: %s", line);
