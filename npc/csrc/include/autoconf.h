493d4d652f1a5cd03a93ebd92b40cda693422abf
tracer-ysyx <tracer@ysyx.org>
2024-07-03 15:06:17 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.32-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 25 May 2024 20:20:51 +0000 x86_64 GNU/Linux  15:06:17 up  4:13,  1 user,  load average: 2.08, 2.04, 1.48

--- npc/csrc/include/autoconf.h
+++ npc/csrc/include/autoconf.h
@@ -2,6 +2,10 @@
 #define CONFIG_ISA "riscv32"
 #define CONFIG_CC_O2 1
 #define CONFIG_CC_OPT "-O2"
+#define CONFIG_TRACE 1
+#define CONFIG_TRACE_START 0
+#define CONFIG_TRACE_END 10000
+#define CONFIG_MTRACE 1
 #define CONFIG_DIFFTEST 1
 #define CONFIG_DIFFTEST_REF_NEMU 1
 #define CONFIG_DIFFTEST_REF_PATH "../nemu/build"
