376218939dc9b345764d790ea1fb1dc679384fda
tracer-ysyx <tracer@ysyx.org>
2024-07-03 18:56:08 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.32-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 25 May 2024 20:20:51 +0000 x86_64 GNU/Linux  18:56:08 up  8:02,  1 user,  load average: 3.25, 2.43, 2.39

--- npc/csrc/cpu/dpi-c/dpi-c.c
+++ npc/csrc/cpu/dpi-c/dpi-c.c
@@ -58,7 +58,7 @@ int pmem_read(int raddr){
     return (uint32_t)(get_time() >> 32);
   }
   //printf("readmem at addr :%x \n",raddr );
-  if(raddr < 0x80000000)
+  if(raddr < CONFIG_MBASE && raddr > CONFIG_MBASE + CONFIG_MSIZE)
     return 0;
   word_t data = host_read(guest_to_host(raddr), 4);
   //printf("pmem_read: raddr = 0x%x, data = 0x%x\n", raddr, data);
