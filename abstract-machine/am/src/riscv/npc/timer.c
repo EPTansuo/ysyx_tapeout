1d89d2b0efba444f84b2269bfaf4fe9c9ebc8071
tracer-ysyx <tracer@ysyx.org>
2024-07-03 17:56:51 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.32-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 25 May 2024 20:20:51 +0000 x86_64 GNU/Linux  17:56:51 up  7:03,  1 user,  load average: 3.38, 2.32, 2.61

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -1,10 +1,11 @@
 #include <am.h>
+#include <riscv/riscv.h>
 
 void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uptime->us = (((uint64_t)inl(0xa0000048+4)<<32) + inl(0xa0000048));
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
