cbea5ade0af3d7daaa9cd64850ef45046810e1ac
tracer-ysyx <tracer@ysyx.org>
2024-07-03 22:04:54 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.32-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 25 May 2024 20:20:51 +0000 x86_64 GNU/Linux  22:04:54 up 11:11,  1 user,  load average: 3.34, 4.30, 4.13

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -35,7 +35,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   return NULL;
 }
-//#undef __riscv_e
+#undef __riscv_e
 void yield() {
 #ifdef __riscv_e
   asm volatile("li a5, -1; ecall");
