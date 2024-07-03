0542f382f55277a23c36f7abb334f6c889a70cf3
tracer-ysyx <tracer@ysyx.org>
2024-07-03 19:57:31 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.32-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 25 May 2024 20:20:51 +0000 x86_64 GNU/Linux  19:57:31 up  9:04,  1 user,  load average: 2.18, 3.01, 3.27

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <riscv/riscv.h>
 #include <klib.h>
+#include <stdio.h>
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
@@ -9,7 +10,7 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       case 11: ev.event = EVENT_YIELD;   break;
-      default: ev.event = EVENT_ERROR;   break;
+      default: ev.event = EVENT_ERROR; printf("mcause=%d\n",c->mcause);   break;
     }
 
     c = user_handler(ev, c);
