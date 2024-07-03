f56dea98cb05fa569bfba17a75d0ab6e1a9eddb1
tracer-ysyx <tracer@ysyx.org>
2024-07-03 22:20:20 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.32-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 25 May 2024 20:20:51 +0000 x86_64 GNU/Linux  22:20:20 up 11:27,  1 user,  load average: 7.04, 5.36, 4.61

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -38,7 +38,8 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 
 void yield() {
 #ifdef __riscv_e
-  asm volatile("li a5, -1; ecall");
+  //asm volatile("li a5, -1; ecall");
+  asm volatile("li a5, 11; ecall");  //根据下面的改成了11，下面的根据spike修改
 #else
   //asm volatile("li a7, -1; ecall");
   asm volatile("li a7, 11; ecall"); //Spike 那边是0xb来着
