f9b08940e15ea6cd58fbb89fb88bfaadfd9f76e2
tracer-ysyx <tracer@ysyx.org>
2024-07-03 18:53:30 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.32-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 25 May 2024 20:20:51 +0000 x86_64 GNU/Linux  18:53:30 up  8:00,  1 user,  load average: 1.79, 1.69, 2.17

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -52,7 +52,7 @@ void __am_disk_blkio(AM_DISK_BLKIO_T *io);
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
 static void __am_uart_config(AM_UART_CONFIG_T *cfg)   { cfg->present = false; }
-static void __am_net_config (AM_NET_CONFIG_T *cfg)    { cfg->present = false; }
+//static void __am_net_config (AM_NET_CONFIG_T *cfg)    { cfg->present = false; }
 
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
@@ -72,7 +72,7 @@ static void *lut[128] = {
 //  [AM_DISK_CONFIG ] = __am_disk_config,
 //  [AM_DISK_STATUS ] = __am_disk_status,
 //  [AM_DISK_BLKIO  ] = __am_disk_blkio,
-  [AM_NET_CONFIG  ] = __am_net_config,
+//  [AM_NET_CONFIG  ] = __am_net_config,
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }
