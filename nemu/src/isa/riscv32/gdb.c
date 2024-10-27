2cc32baa5838046d64175463e3f92b0365d26ad8
tracer-ysyx <tracer@ysyx.org>
2024-10-27 14:07:05 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.54-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 05 Oct 2024 17:53:31 +0000 x86_64 GNU/Linux  14:07:05 up 2 days, 20:59,  2 users,  load average: 7.52, 5.94, 4.37

--- nemu/src/isa/riscv32/gdb.c
+++ nemu/src/isa/riscv32/gdb.c
@@ -121,9 +121,9 @@ bool init_gdbstub()
             .reg_byte = 4,
             .reg_num = 32,
 #ifdef CONFIG_RV64
-            .target_desc = TARGET_RV32,
-#else
             .target_desc = TARGET_RV64,
+#else
+            .target_desc = TARGET_RV32,
 #endif
         };
         char ip_port[32];
