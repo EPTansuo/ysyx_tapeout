a0721a7f0d3bea9131dab71550b966848ae903fa
tracer-ysyx <tracer@ysyx.org>
2024-10-27 16:14:38 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.54-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 05 Oct 2024 17:53:31 +0000 x86_64 GNU/Linux  16:14:38 up  1:23,  2 users,  load average: 1.98, 2.30, 2.31

--- nemu/src/isa/riscv32/gdb.c
+++ nemu/src/isa/riscv32/gdb.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <utils.h>
 #include <fmt-def.h>
+#include <errno.h>
 
 
 void execute(uint64_t n);
@@ -42,7 +43,7 @@ static int nemu_read_mem(void *args, size_t addr, size_t len, void *val)
         printf(":---read_mem = "FMT_WORD_HEX" %zx\n", (word_t)addr, len);
         if(addr < 0x80000000 || addr > 0x80000000 + 0x1000000){
                 printf(":---ERROR: read_mem = "FMT_WORD_HEX" %zx\n", (word_t)addr, len);
-                return -1;
+                return EFAULT;
         }
         
         (*((word_t*)val)) = vaddr_read(addr, len);
