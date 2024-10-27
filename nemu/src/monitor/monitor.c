21ebecabb29ac22ccf17728c0f284e46d1dfe9ec
tracer-ysyx <tracer@ysyx.org>
2024-10-27 14:57:48 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.54-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 05 Oct 2024 17:53:31 +0000 x86_64 GNU/Linux  14:57:48 up 6 min,  2 users,  load average: 2.09, 1.31, 0.65

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -46,7 +46,7 @@ void sdb_set_batch_mode();
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 //static char *img_file =  NULL;
-static char *img_file =  "/home/han/Disk/Document/PROJECT/ysyx/ysyx-workbench/am-kernels/tests/am-tests/build/amtest-riscv32e-nemu.bin";
+static char *img_file =  "/home/han/Disk/Document/PROJECT/ysyx/ysyx-workbench/am-kernels/tests/cpu-tests/build/mul-longlong-riscv32-nemu.bin";
 static int difftest_port = 1234;
 
 static long load_img() {
