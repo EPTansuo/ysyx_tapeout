3bfcfdb4161d1230cfd1f5f4c6b8f9c82fe54d4f
tracer-ysyx <tracer@ysyx.org>
2024-10-27 15:26:37 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.54-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 05 Oct 2024 17:53:31 +0000 x86_64 GNU/Linux  15:26:37 up 35 min,  2 users,  load average: 1.49, 1.29, 1.08

--- nemu/src/isa/riscv32/gdb.c
+++ nemu/src/isa/riscv32/gdb.c
@@ -157,7 +157,7 @@ bool init_gdbstub()
 
         size_t reg_value;
         //printf("function: read_reg: %p\n", nemu_read_reg);
-        int ret = (gdbstub.ops->read_reg)(NULL, 0, &reg_value);
+        int ret = (ops.read_reg)(NULL, 0, &reg_value);
 
         printf("ret: %d\n", ret);
         if(!gdbstub_run(&gdbstub, NULL)){
