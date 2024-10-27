046d8a222f579d02ff3222e7ffd7162133212a71
tracer-ysyx <tracer@ysyx.org>
2024-10-27 15:23:18 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.54-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 05 Oct 2024 17:53:31 +0000 x86_64 GNU/Linux  15:23:18 up 32 min,  2 users,  load average: 2.27, 1.32, 1.05

--- nemu/src/isa/riscv32/gdb.c
+++ nemu/src/isa/riscv32/gdb.c
@@ -155,8 +155,11 @@ bool init_gdbstub()
         
         print_gdbstub(&gdbstub);
 
+        size_t reg_value;
         //printf("function: read_reg: %p\n", nemu_read_reg);
+        int ret = (gdbstub.ops->read_reg)(NULL, 0, &reg_value);
 
+        printf("ret: %d\n", ret);
         if(!gdbstub_run(&gdbstub, NULL)){
                 fprintf(stderr, "Fail to run in debug mode.\n");
                 return false;
