5d7abb202b3fd5088bcc8f77d73ad5f5e199f72b
tracer-ysyx <tracer@ysyx.org>
2024-11-10 10:07:19 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.59-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 06:29:43 +0000 x86_64 GNU/Linux  10:07:19 up 20:38,  2 users,  load average: 4.14, 5.39, 5.82

--- nemu/src/isa/riscv32/gdb.c
+++ nemu/src/isa/riscv32/gdb.c
@@ -92,7 +92,7 @@ static int nemu_write_mem(void *args, size_t addr, size_t len, void *val)
 
 gdb_action_t nemu_cont(void *args)
 {
-        cpu_exec(-1);
+        execute(-1);
         print_nemu_state();
         printf(":---continue over\n");
         return ACT_RESUME;
