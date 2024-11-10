7b1ba90ac46f1ed941022b2cabe81419b5337443
tracer-ysyx <tracer@ysyx.org>
2024-11-10 10:24:00 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.59-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 06:29:43 +0000 x86_64 GNU/Linux  10:24:00 up 20:55,  2 users,  load average: 5.57, 4.14, 4.51

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -84,7 +84,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 
 
   scan_watchpoint();
-  scan_breakpoint(_this->snpc);
+  scan_breakpoint(_this->pc);
+  printf("pc: 0x" FMT_WORD_HEX " ", _this->pc);
+  printf("npc: 0x" FMT_WORD_HEX "\n", dnpc);
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
