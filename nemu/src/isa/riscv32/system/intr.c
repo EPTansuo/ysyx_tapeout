a93b2952110657cf890c910799d39d15c6451ac4
tracer-ysyx <tracer@ysyx.org>
2024-11-16 10:38:15 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  10:38:15 up  1:29,  2 users,  load average: 2.46, 2.27, 2.25

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -26,10 +26,10 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   //   epc += 4;
   
 
-  printf("NO: %d\n", NO);
+  printf("\nNO: %d\n", NO);
   cpu.csr.mcause = NO; 
   cpu.csr.mepc = epc;
-  cpu.csr.mstatus = 0x1800;
+ // cpu.csr.mstatus = 0x1800;
   return cpu.csr.mtvec;
 }
 
