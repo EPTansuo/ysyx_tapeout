4b491c04db409103276bcaf654d22fa7fc6ded30
tracer-ysyx <tracer@ysyx.org>
2024-11-16 15:58:46 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  15:58:46 up  6:49,  2 users,  load average: 1.94, 2.52, 2.77

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -12,16 +12,16 @@ Context* __am_irq_handle(Context *c) {
       case 11: ev.event = EVENT_YIELD; c->mepc+=4;  break;
       default: ev.event = EVENT_ERROR;   break;
     }
-    // printf("before:\n");
-    // printf("ctx->mepc=%x\n",c->mepc);
-    // printf("ctx->mcause=%x\n",c->mcause);
-    // printf("ctx->mstatus=%x\n",c->mstatus);
-    // printf("ctx->gpr[10]=%x\n",c->gpr[10]);
+    printf("before:\n");
+    printf("ctx->mepc=%x\n",c->mepc);
+    printf("ctx->mcause=%x\n",c->mcause);
+    printf("ctx->mstatus=%x\n",c->mstatus);
+    printf("ctx->gpr[10]=%x\n",c->gpr[10]);
     c = user_handler(ev, c);
-    // printf("ctx->mepc=%x\n",c->mepc);
-    // printf("ctx->mcause=%x\n",c->mcause);
-    // printf("ctx->mstatus=%x\n",c->mstatus);
-    // printf("ctx->gpr[10]=%x\n",c->gpr[10]);
+    printf("ctx->mepc=%x\n",c->mepc);
+    printf("ctx->mcause=%x\n",c->mcause);
+    printf("ctx->mstatus=%x\n",c->mstatus);
+    printf("ctx->gpr[10]=%x\n",c->gpr[10]);
     assert(c != NULL);
   }
 
