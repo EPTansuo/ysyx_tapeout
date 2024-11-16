d58684fa33fa897c825b608b5be1bcfacde6bfea
tracer-ysyx <tracer@ysyx.org>
2024-11-16 11:36:52 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  11:36:52 up  2:27,  2 users,  load average: 4.22, 3.16, 2.66

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -12,9 +12,16 @@ Context* __am_irq_handle(Context *c) {
       case 11: ev.event = EVENT_YIELD;   break;
       default: ev.event = EVENT_ERROR;   break;
     }
-    printf("ctx->mepc_before=%d\n",c->mepc);
+    printf("before:\n");
+    printf("ctx->mepc=%d\n",c->mepc);
+    printf("ctx->mcause=%d\n",c->mcause);
+    printf("ctx->mstatus=%d\n",c->mstatus);
+    printf("ctx->gpr[10]=%d\n",c->gpr[10]);
     c = user_handler(ev, c);
     printf("ctx->mepc=%d\n",c->mepc);
+    printf("ctx->mcause=%d\n",c->mcause);
+    printf("ctx->mstatus=%d\n",c->mstatus);
+    printf("ctx->gpr[10]=%d\n",c->gpr[10]);
     assert(c != NULL);
   }
 
