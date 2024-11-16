03de2c7953f236ba19bafbb2124f9d9390df2c48
tracer-ysyx <tracer@ysyx.org>
2024-11-16 15:08:41 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  15:08:41 up  5:59,  2 users,  load average: 0.53, 1.90, 2.38

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -8,11 +8,19 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 11: ev.event = EVENT_YIELD; break;  //因为spike上面是11，所以都改成了11
-      default: ev.event = EVENT_ERROR; break;
+      case 11: ev.event = EVENT_YIELD; c->mepc+=4;  break;
+      default: ev.event = EVENT_ERROR;   break;
     }
-
+    // printf("before:\n");
+    // printf("ctx->mepc=%x\n",c->mepc);
+    // printf("ctx->mcause=%x\n",c->mcause);
+    // printf("ctx->mstatus=%x\n",c->mstatus);
+    // printf("ctx->gpr[10]=%x\n",c->gpr[10]);
     c = user_handler(ev, c);
+    // printf("ctx->mepc=%x\n",c->mepc);
+    // printf("ctx->mcause=%x\n",c->mcause);
+    // printf("ctx->mstatus=%x\n",c->mstatus);
+    // printf("ctx->gpr[10]=%x\n",c->gpr[10]);
     assert(c != NULL);
   }
 
@@ -32,8 +40,19 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+  //Context *ctx = kstack.end - sizeof(Context);
+
+  uintptr_t stack_top = (uintptr_t)kstack.end;
+  stack_top -= stack_top % sizeof(uintptr_t); //对齐
+  stack_top -= sizeof(Context);// 为Context结构体留出空间
+  Context* ctx = (Context *)stack_top;
+  ctx->mepc=(uintptr_t)entry;
+  ctx->mstatus=0x1800;
+  ctx->gpr[10]=(uintptr_t)arg;  //a0
+  ctx->gpr[2]=stack_top; //sp
+  return ctx;
 }
+
 
 void yield() {
 #ifdef __riscv_e
