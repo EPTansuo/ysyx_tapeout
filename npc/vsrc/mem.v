3f0338f2c8933abd07a0e34fced41ae3e28ec770
tracer-ysyx <tracer@ysyx.org>
2024-11-16 14:43:46 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  14:43:46 up  5:34,  2 users,  load average: 4.30, 2.55, 1.79

---
+++ npc/vsrc/mem.v
@@ -0,0 +1,45 @@
+`include "defines.v"
+`include "inst_def.v"
+
+
+module mem(
+        input clk,
+        input rst,
+
+        //from cpu.exu
+        input wire we,
+        input wire re,
+        input wire [`InstAddrBus] w_addr,
+        input wire [`InstAddrBus] r_addr,
+        input wire [`WordBus] w_data,
+
+        input wire [7:0] w_mask,
+        input wire [7:0] r_mask,
+        
+        //transfer to cpu.exu
+        output reg [`WordBus] r_data
+);
+
+
+import "DPI-C" function int pmem_read(input int raddr);
+import "DPI-C" function void pmem_write(input int waddr, input int wdata, input byte wmask);
+
+
+
+
+always @(*) begin
+        if (rst == `RstDisable) begin // 有读写请求时
+          r_data = pmem_read(r_addr);
+          if (we) begin // 有写请求时
+            pmem_write(w_addr, w_data, w_mask);
+          end
+        end
+        else begin
+          r_data = 0;
+        end
+      end
+
+
+endmodule
+
+
