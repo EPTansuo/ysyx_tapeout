6c8078bbecfef972c036393147f3873429d17230
tracer-ysyx <tracer@ysyx.org>
2024-11-16 09:53:42 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  09:53:42 up 44 min,  2 users,  load average: 4.61, 3.40, 2.33

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 11: ev.event = EVENT_YIELD;   break;
+      case 11: ev.event = EVENT_YIELD; c->mepc+=4;  break;
       default: ev.event = EVENT_ERROR;   break;
     }
 
@@ -48,7 +48,7 @@ void yield() {
   asm volatile("li a5, 11; ecall");  //根据下面的改成了11，下面的根据spike修改
 #else
   //asm volatile("li a7, -1; ecall");
-  asm volatile("li a7, 11; ecall"); //Spike 那边是0xb来着
+  asm volatile("li a7, -1; ecall"); //Spike 那边是0xb来着
 #endif
 }
 
