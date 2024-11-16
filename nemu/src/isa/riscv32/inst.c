7206278f0cc3700154b05b1479c637a9b036603e
tracer-ysyx <tracer@ysyx.org>
2024-11-16 16:10:34 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  16:10:34 up  7:01,  2 users,  load average: 2.26, 2.37, 2.53

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -32,7 +32,9 @@ static vaddr_t *csr_register(word_t imm) {
   }
 }
 
-#define ECALL(dnpc) { bool success; dnpc = (isa_raise_intr(isa_reg_str2val("a7", &success), s->pc)); }
+#define ECALL(dnpc) { bool success; dnpc = (\
+isa_raise_intr(isa_reg_str2val(MUXDEF(CONFIG_RVE,"a5","a7"), &success), s->pc)); }
+
 #define CSR(i) *csr_register(i)
 
 
