3c943881585d40c0f2210cdd9333f8af39c08be2
tracer-ysyx <tracer@ysyx.org>
2024-11-21 14:53:07 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  14:53:07 up  5:10,  2 users,  load average: 2.76, 1.83, 1.54

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -118,17 +118,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
     }
 
 #endif
-printf("pmem_read(addr, len) = 0x%08x\n", pmem_read(addr, len)) ;
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
-
-  printf("pmem_read(addr, len) = 0x%08x\n", pmem_read(addr, len));
-  if(pmem_read(addr, len) != data){
-    printf("pmem_read != data_write\n");
-    printf("addr = 0x%08x, len = %d, data = 0x%08x\n", addr, len, data);
-    printf("pmem_read(addr, len) = 0x%08x\n", pmem_read(addr, len));
-    assert(0);
-  }
-
 }
