8e5f0f08aa75af784288aed095f7d981d2e565c8
tracer-ysyx <tracer@ysyx.org>
2024-11-21 10:34:50 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  10:34:50 up 52 min,  2 users,  load average: 1.75, 1.55, 1.34

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -112,6 +112,10 @@ void paddr_write(paddr_t addr, int len, word_t data) {
     if(addr == 0xa00003f8) {   // SERIAL
       return;  
     }
+    else if(addr >= 0x10000100 && addr <= 0xa0000107){ //VGA
+      return;
+    }
+
 #endif
 
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
