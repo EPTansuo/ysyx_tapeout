542587e226174f2f7cc9e5aea7ced453b011ab10
tracer-ysyx <tracer@ysyx.org>
2024-11-21 18:29:37 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  18:29:37 up 8 min,  2 users,  load average: 0.84, 1.17, 0.73

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -78,7 +78,7 @@ void init_mem() {
   assert(pmem);
 #endif
   IFDEF(CONFIG_MEM_RANDOM, memset(pmem, rand(), CONFIG_MSIZE));
-  memset(pmem,0x13,CONFIG_MSIZE);
+  //memset(pmem,0x13,CONFIG_MSIZE);
   Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
 }
 
