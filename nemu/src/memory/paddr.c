dfeb02cb26b320eb38fe4bcd77a49f4f0f6cd863
tracer-ysyx <tracer@ysyx.org>
2024-11-21 10:33:04 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  10:33:04 up 50 min,  2 users,  load average: 1.61, 1.56, 1.32

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -108,6 +108,12 @@ void paddr_write(paddr_t addr, int len, word_t data) {
 #ifdef CONFIG_MTRACE
   print_memwrite(addr, len, data);
 #endif
+#ifdef CONFIG_TARGET_SHARE
+    if(addr == 0xa00003f8) {   // SERIAL
+      return;  
+    }
+#endif
+
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
