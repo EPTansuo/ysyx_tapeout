6953e55376f773e8d6fed2e2fd5631cb83bc0f25
EPTansuo <eptansuo@163.com>
2024-11-24 19:31:34 +0800

    fixed git error

--- npc/csrc/include/autoconf.h
+++ npc/csrc/include/autoconf.h
@@ -1,8 +1,8 @@
 #define CONFIG_ISA_riscv 1
 #define CONFIG_ISA "riscv32"
 #define CONFIG_RVE 1
-#define CONFIG_CC_O0 1
-#define CONFIG_CC_OPT "-O0"
+#define CONFIG_CC_O2 1
+#define CONFIG_CC_OPT "-O2"
 #define CONFIG_WAVE_DUMP 1
 #define CONFIG_TRACE 1
 #define CONFIG_TRACE_START 0
