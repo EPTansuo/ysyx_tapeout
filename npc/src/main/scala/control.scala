24eda5d3445fec0f10ed77226b201747a2cecfd1
tracer-ysyx <tracer@ysyx.org>
2024-11-24 17:39:30 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  17:39:30 up 23:54,  2 users,  load average: 5.19, 3.96, 3.60

--- npc/src/main/scala/control.scala
+++ npc/src/main/scala/control.scala
@@ -85,7 +85,7 @@ val map = Array(
         lui     ->  List(PC_4  , A_RS1, B_IMM, ALU_COPY_B, IMM_U, WB_ALU, LD_XX, ST_XX, MASK_XX, INST_VALID  ),
         auipc   ->  List(PC_4  , A_PC , B_IMM, ALU_ADD   , IMM_U, WB_ALU, LD_XX, ST_XX, MASK_XX, INST_VALID  ),
         addi    ->  List(PC_4  , A_RS1, B_IMM, ALU_ADD   , IMM_I, WB_ALU, LD_XX, ST_XX, MASK_XX, INST_VALID  ),
-        jalr    ->  List(PC_ALU, A_RS1, B_IMM, ALU_ADD   , IMM_I, WB_PC4, LD_XX, ST_XX, MASK_XX, INST_INVALID),
+        jalr    ->  List(PC_ALU, A_RS1, B_IMM, ALU_ADD   , IMM_I, WB_PC4, LD_XX, ST_XX, MASK_XX, INST_VALID),
         jal     ->  List(PC_ALU, A_PC , B_IMM, ALU_ADD   , IMM_J, WB_PC4, LD_XX, ST_XX, MASK_XX, INST_VALID  ),
         sw      ->  List(PC_4  , A_RS1, B_IMM, ALU_ADD   , IMM_S, WB_XX , LD_XX, ST_SW, MASK_W , INST_VALID  ),
         lw      ->  List(PC_4  , A_RS1, B_IMM, ALU_ADD   , IMM_I, WB_XX , LD_LW, ST_XX, MASK_XX, INST_VALID  ),
