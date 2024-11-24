a7cf55bc9184472ca3fff44d5980ae65a5055f25
tracer-ysyx <tracer@ysyx.org>
2024-11-24 18:28:29 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  18:28:29 up 1 day, 43 min,  2 users,  load average: 4.28, 3.54, 3.38

--- npc/src/main/scala/sigmap.scala
+++ npc/src/main/scala/sigmap.scala
@@ -35,7 +35,7 @@ val map = Array(
 	jal     ->  List(PC_ALU, A_PC , B_IMM, ALU_ADD   , IMM_J, WB_PC4, LD_XX, ST_XX, MASK_XX, BR_XX , INST_VALID  ),
 	sw      ->  List(PC_4  , A_RS1, B_IMM, ALU_ADD   , IMM_S, WB_XX , LD_XX, ST_SW, MASK_W , BR_XX , INST_VALID  ),
 	lw      ->  List(PC_4  , A_RS1, B_IMM, ALU_ADD   , IMM_I, WB_XX , LD_LW, ST_XX, MASK_XX, BR_XX , INST_VALID  ),
-	bgeu    ->  List(PC_ALU, A_PC , B_IMM, ALU_ADD   , IMM_I, WB_XX , LD_XX, ST_XX, MASK_XX, BR_GEU, INST_VALID  ),
+	bgeu    ->  List(PC_ALU, A_PC , B_IMM, ALU_ADD   , IMM_B, WB_XX , LD_XX, ST_XX, MASK_XX, BR_GEU, INST_VALID  ),
 )
 val 
 	default  =  List(PC_4  , A_RS1, B_IMM, ALU_ADD   , IMM_I, WB_XX , LD_XX, ST_XX, MASK_XX, BR_XX , INST_INVALID)
