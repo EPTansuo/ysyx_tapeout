66975fb2c8dde1c358c225138d36f62e064f10c8
tracer-ysyx <tracer@ysyx.org>
2024-11-24 17:34:46 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  17:34:46 up 23:49,  2 users,  load average: 3.75, 2.83, 3.20

--- npc/src/main/scala/immgen.scala
+++ npc/src/main/scala/immgen.scala
@@ -18,13 +18,13 @@ class ImmGen(xlen: Int) extends Module{
     val io = IO(new ImmGenIO(xlen))
 
 
-    val immI = io.inst(31, 20).asUInt
-    val immU = Cat(io.inst(31, 12), 0.U(12.W)).asUInt
-    val immS = Cat(io.inst(31, 25), io.inst(11, 7)).asUInt
-    val immJ = Cat(io.inst(31), io.inst(19, 12), io.inst(20), io.inst(30, 25), io.inst(24, 21), 0.U(1.W)).asUInt
-    val immB = Cat(io.inst(31), io.inst(7), io.inst(30, 25), io.inst(11, 8), 0.U(1.W)).asUInt
+    val immI = io.inst(31, 20).asSInt
+    val immU = Cat(io.inst(31, 12), 0.U(12.W)).asSInt
+    val immS = Cat(io.inst(31, 25), io.inst(11, 7)).asSInt
+    val immJ = Cat(io.inst(31), io.inst(19, 12), io.inst(20), io.inst(30, 25), io.inst(24, 21), 0.U(1.W)).asSInt
+    val immB = Cat(io.inst(31), io.inst(7), io.inst(30, 25), io.inst(11, 8), 0.U(1.W)).asSInt
     import imm_sel._
-    io.out := MuxLookup(io.sel, 0.U, Seq(
+    io.out := MuxLookup(io.sel, 0.S, Seq(
             IMM_I -> immI,
             IMM_U -> immU,
             IMM_S -> immS,
