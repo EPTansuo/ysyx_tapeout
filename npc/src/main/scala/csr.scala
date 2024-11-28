73a5aae57ab8ab033b01636570ee03c343ef355d
tracer-ysyx <tracer@ysyx.org>
2024-11-28 19:23:00 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  19:23:00 up  3:42,  2 users,  load average: 3.63, 1.69, 1.22

--- npc/src/main/scala/csr.scala
+++ npc/src/main/scala/csr.scala
@@ -76,11 +76,11 @@ class CSR(xlen:Int) extends Module{
     io.epc := mtvec
   }.elsewhen(is_mret){
       mstatus := Cat(mstatus(31, 13), 
-                 2.U(2.W),            
+                 3.U(2.W),            
                  mstatus(10, 8),  
-                 mstatus(3),      
+                 mstatus(3),      // MPIE
                  mstatus(6, 4),   
-                 0.U(1.W),        
+                 0.U(1.W),        // MIE
                  mstatus(2, 0))   
     io.epc := mepc
   }.otherwise{
