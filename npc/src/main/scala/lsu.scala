4ce008341311e9a0a2bc7315308e6157f1c3ba5d
tracer-ysyx <tracer@ysyx.org>
2024-11-30 16:11:31 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  16:11:31 up 2 days, 30 min,  2 users,  load average: 3.90, 1.99, 1.26

--- npc/src/main/scala/lsu.scala
+++ npc/src/main/scala/lsu.scala
@@ -32,15 +32,21 @@ class LSU(xlen: Int) extends Module {
     val wbu_data = in_reg.bits.wbu
     val npc = in_reg.bits.npc
 
-    val fsm_s = Module(new ComFSM_S)
-    val state_s = fsm_s.io.state
-    fsm_s.io.valid := io.in.valid
-    fsm_s.io.ready := io.in.ready
+    // val fsm_s = Module(new ComFSM_S)
+    // val state_s = fsm_s.io.state
+    // fsm_s.io.valid := io.in.valid
+    // fsm_s.io.ready := io.in.ready
 
-    when(state_s === read_s){
+    // when(state_s === read_s){
+    //     in_reg := io.in
+    // }
+    // io.in.ready := state_s === read_s
+
+    io.in.ready := io.in.valid
+    when(io.in.ready){
         in_reg := io.in
     }
-    io.in.ready := state_s === read_s
+
 
     val fsm_m = Module(new ComFSM_M)
     val state_m = fsm_m.io.state
