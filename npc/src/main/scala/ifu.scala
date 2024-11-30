0b1b65806759dbca3916b1d01888e347c02eacf4
tracer-ysyx <tracer@ysyx.org>
2024-11-30 12:02:37 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  12:02:37 up 1 day, 20:21,  2 users,  load average: 1.59, 1.44, 1.57

--- npc/src/main/scala/ifu.scala
+++ npc/src/main/scala/ifu.scala
@@ -3,8 +3,8 @@ package cpu
 import chisel3._
 import chisel3.util._
 import defines._ 
-import state_m._
-import state_s._
+// import state_m._
+// import state_s._
 
 class IFU(xlen:Int) extends Module {
   val io = IO(new Bundle { 
@@ -15,19 +15,28 @@ class IFU(xlen:Int) extends Module {
     val mem_inst = Input(UInt(32.W))
   })
 
-  val pc = Reg(UInt(xlen.W))
+  //val pc = Reg(UInt(xlen.W))
   
-  val fsm_m = Module(new ComFSM_M)
-  val state_m = fsm_m.io.state
-  fsm_m.io.valid := io.out.valid
-  fsm_m.io.ready := io.out.ready
 
-  when(state_m === idle_m){
-    pc := io.pc_in
-    io.out.valid := 1.U
-  }.otherwise{
-    io.out.valid := 0.U
-  }
+  val idle::wait_ready::Nil = Enum(2)
+  val state = RegInit(idle)
+  state := MuxLookup(state, idle, Seq(
+    (idle -> Mux(io.out.ready, wait_ready, idle)),
+    (wait_ready -> Mux(io.out.valid, idle, wait_ready)
+  )))
+  io.out.valid := state === idle
+  val pc =io.pc_in
+  // val fsm_m = Module(new ComFSM_M)
+  // val state_m = fsm_m.io.state
+  // fsm_m.io.valid := io.out.valid
+  // fsm_m.io.ready := io.out.ready
+
+  // when(state_m === idle_m){
+  //   pc := io.pc_in
+  //   io.out.valid := 1.U
+  // }.otherwise{
+  //   io.out.valid := 0.U
+  // }
 
 
   io.mem_pc := pc
