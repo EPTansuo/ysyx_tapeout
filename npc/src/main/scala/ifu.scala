8c40f9eb86f926e0d30fb8a68bed290ff7def0f8
tracer-ysyx <tracer@ysyx.org>
2024-11-30 13:24:15 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  13:24:15 up 1 day, 21:43,  2 users,  load average: 2.94, 2.38, 2.20

--- npc/src/main/scala/ifu.scala
+++ npc/src/main/scala/ifu.scala
@@ -24,7 +24,7 @@ class IFU(xlen:Int) extends Module {
   val fsm_m = Module(new ComFSM_M)
   val state_m = fsm_m.io.state
   fsm_m.io.valid := io.out.valid
-  fsm_m.io.ready := io.out.ready
+  fsm_m.io.ready := 1.U
 
   io.out.valid := state_m === write_m
 
