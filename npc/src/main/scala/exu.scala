e6251c63ab73693277a0c9f5bf079578cf894cc6
tracer-ysyx <tracer@ysyx.org>
2024-11-30 16:09:03 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  16:09:03 up 2 days, 28 min,  2 users,  load average: 2.10, 1.39, 1.01

--- npc/src/main/scala/exu.scala
+++ npc/src/main/scala/exu.scala
@@ -33,15 +33,20 @@ class EXU(xlen: Int) extends Module{
     val inst = in_reg.bits.inst
     val ctrlsig = in_reg.bits.exu
 
-    val fsm_s = Module(new ComFSM_S)
-    val state_s = fsm_s.io.state
-    fsm_s.io.valid := io.in.valid
-    fsm_s.io.ready := io.in.ready
+    // val fsm_s = Module(new ComFSM_S)
+    // val state_s = fsm_s.io.state
+    // fsm_s.io.valid := io.in.valid
+    // fsm_s.io.ready := io.in.ready
 
-    when(state_s === read_s){
+    // when(state_s === read_s){
+    //     in_reg := io.in
+    // }
+    // io.in.ready := state_s === read_s
+
+    io.in.ready := io.in.valid
+    when(io.in.ready){
         in_reg := io.in
     }
-    io.in.ready := state_s === read_s
 
     val fsm_m = Module(new ComFSM_M)
     val state_m = fsm_m.io.state
