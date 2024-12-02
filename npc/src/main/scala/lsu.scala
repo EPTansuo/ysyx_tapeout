f2eda6b4f670a09d8a44f4cad2bf6e0a2c71a09f
tracer-ysyx <tracer@ysyx.org>
2024-12-02 12:27:53 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  12:27:53 up 3 days, 20:47,  2 users,  load average: 3.92, 2.89, 2.27

--- npc/src/main/scala/lsu.scala
+++ npc/src/main/scala/lsu.scala
@@ -36,7 +36,7 @@ class LSU(xlen: Int) extends Module {
 
     val state = RegInit(s_idle)         
     state := MuxLookup(state, s_idle, Seq(
-        s_idle -> Mux(load_en, s_read, Mux(store_en, s_write, s_idle)),
+        s_idle -> Mux(load_en, s_read, Mux(store_en, s_write, Mux(io.in.valid, s_wait_ready, s_idle))),
         s_read -> Mux(io.dmem.ar.ready, s_wait_ready, s_read),
         s_write -> Mux(io.dmem.aw.ready && io.dmem.ar.ready, s_wait_write, s_write),
         s_wait_read  -> Mux(io.dmem.r.valid,s_wait_ready, s_wait_read),
