391de05049e2ea4ab19fc7e402011baf1b05f595
EPTansuo <eptansuo@163.com>
2024-12-02 19:47:51 +0800

    add clint, but not support difftest

--- npc/src/main/scala/top.scala
+++ npc/src/main/scala/top.scala
@@ -16,13 +16,15 @@ class npc extends Module {
   val cpu = Module(new ysyx_npc(32))
   val sram = Module(new SRAM)
   val uart = Module(new UART_AXILite)
+  val clint = Module(new CLINT)
 
-  val axi_xbar = Module(new AXILiteXbar(2, 
-                        Array((0L,0xFFFFFFFFL),(0xa0000048L,0xa000004FL)),
+  val axi_xbar = Module(new AXILiteXbar(3, 
+                        Array((0L,0xFFFFFFFFL),(0xa0000048L,0xa000004FL),(0xa0000048L,0xa0000056L)),
                         32,32))
   axi_xbar.io.in <> cpu.io.axi
   axi_xbar.io.out(0) <> sram.io.axi
   axi_xbar.io.out(1) <> uart.io.axi
+  axi_xbar.io.out(2) <> clint.io.axi  
 }
 
 object npcMain extends App {
