f5cb5df9483434fdfaf7fe60bba4b745b050a3d7
tracer-ysyx <tracer@ysyx.org>
2024-12-02 19:10:41 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  19:10:41 up 4 days,  3:30,  2 users,  load average: 1.26, 2.00, 1.90

--- npc/vsrc/uart.v
+++ npc/vsrc/uart.v
@@ -5,9 +5,12 @@ module UART(
     input [7:0]data
 );
 
+import "DPI-C" function void pmem_write(input int waddr, input int wdata, input byte wmask);
+
 always@(posedge clock) begin
     if(en) begin
-        $write("%c",data);
+        //$write("%c",data);
+        pmem_write(32'ha00003f8, {24'h0,data}, 8'b0000_0001);
     end
 end
 
