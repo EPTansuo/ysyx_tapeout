a0034fa89cc1780a03903d05cec3da0232b4fdbd
EPTansuo <eptansuo@163.com>
2024-12-03 17:42:19 +0800

    support ysyxsoc am

---
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -0,0 +1,35 @@
+AM_SRCS := riscv/ysyxsoc/start.S \
+           riscv/ysyxsoc/trm.c \
+           riscv/ysyxsoc/ioe.c \
+           riscv/ysyxsoc/timer.c \
+           riscv/ysyxsoc/input.c \
+           riscv/ysyxsoc/cte.c \
+           riscv/ysyxsoc/trap.S \
+           riscv/ysyxsoc/gpu.c \
+           platform/dummy/vme.c \
+           platform/dummy/mpe.c
+# gpu.c 是自己加的
+
+CFLAGS    += -fdata-sections -ffunction-sections
+LDFLAGS   += -T $(AM_HOME)/scripts/linker_ysyxsoc.ld \
+						 --defsym=_mrom_start=0x20000000 --defsym=_sram_start=0x0f000000
+LDFLAGS   += --gc-sections -e _start
+CFLAGS += -DMAINARGS=\"$(mainargs)\"
+
+NPCFLAGS =--diff=$(NEMU_HOME)/build/riscv32-nemu-interpreter-so 
+
+BATCH ?= 0
+ifeq ($(BATCH), 1)
+    NPCFLAGS += -b
+endif
+
+
+.PHONY: $(AM_HOME)/am/src/riscv/ysyxsoc/trm.c
+
+image: $(IMAGE).elf
+	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
+run:  image
+	$(MAKE) -C $(NPC_HOME) run  ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin
--- npc/csrc/cpu/dpi-c/dpi-c.c
+++ npc/csrc/cpu/dpi-c/dpi-c.c
@@ -168,7 +168,7 @@ extern "C" void flash_read(int32_t addr, int32_t *data) { assert(0); }
 extern "C" void mrom_read(int32_t addr, int32_t *data) { 
   //*data =0x100073; // ebreak
 
-  *data = pmem_read(addr + 0x60000000);
+  *data = pmem_read(addr);
  // *data = 0x00e78023;
 
 }
