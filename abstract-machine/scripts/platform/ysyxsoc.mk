181c7b2719185450b3bc9f5a530e76c1543bdc27
tracer-ysyx <tracer@ysyx.org>
2024-12-03 15:13:08 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  15:13:08 up 4 days, 23:32,  2 users,  load average: 2.15, 1.77, 2.18

---
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -0,0 +1,35 @@
+AM_SRCS := riscv/ysyxsoc/start.S \
+           riscv/ysyxsoc/trm.c \
+           riscv/ysyxsoc/ioe.c \
+           riscv/ysyxsoc/timer.c \
+           riscv/ysyxsoc/input.c \
+           riscv/ysyxsoc/cte.c \
+           riscv/ysyxsoc/trap.S \
+           riscv/ysyxsoc/gpu.c \
+           platform/dummy/vme.c \
+           platform/dummy/mpe.c
+# gpu.c 是自己加的
+
+CFLAGS    += -fdata-sections -ffunction-sections
+LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
+						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
+LDFLAGS   += --gc-sections -e _start
+CFLAGS += -DMAINARGS=\"$(mainargs)\"
+
+NPCFLAGS =--diff=$(NEMU_HOME)/build/riscv32-nemu-interpreter-so 
+
+BATCH ?= 0
+ifeq ($(BATCH), 1)
+    NPCFLAGS += -b
+endif
+
+
+.PHONY: $(AM_HOME)/am/src/riscv/ysyxsoc/trm.c
+
+image: $(IMAGE).elf
+	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
+run:  image
+	$(MAKE) -C $(NPC_HOME) run  ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin
