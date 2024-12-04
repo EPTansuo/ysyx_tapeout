6844c752e5773aef860694faf3da170d23661de7
tracer-ysyx <tracer@ysyx.org>
2024-12-04 14:15:56 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  14:15:56 up 5 days, 22:35,  2 users,  load average: 5.96, 3.92, 2.96

--- npc/csrc/cpu/dpi-c/dpi-c.c
+++ npc/csrc/cpu/dpi-c/dpi-c.c
@@ -164,7 +164,11 @@ end_pmem_write:
 
 
 
-extern "C" void flash_read(int32_t addr, int32_t *data) { assert(0); }
+extern "C" void flash_read(int32_t addr, int32_t *data) { 
+  
+
+
+}
 extern "C" void mrom_read(int32_t addr, int32_t *data) { 
   //*data =0x100073; // ebreak
 
