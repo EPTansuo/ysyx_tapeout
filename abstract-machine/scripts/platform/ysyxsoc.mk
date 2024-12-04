3e4491088f5d9c966b4c27414cbf2eaaad334c30
tracer-ysyx <tracer@ysyx.org>
2024-12-04 09:58:09 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  09:58:09 up 5 days, 18:17,  2 users,  load average: 1.15, 0.89, 0.88

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -13,7 +13,7 @@ AM_SRCS := riscv/ysyxsoc/start.S \
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker_ysyxsoc.ld
 
-LDFLAGS   += --gc-sections -e _start
+LDFLAGS   += --gc-sections -e _start --print-map
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 
 NPCFLAGS =--diff=$(NEMU_HOME)/build/riscv32-nemu-interpreter-so 
