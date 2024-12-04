fc82156858c154abf2995ce98a01738de6cc3bfb
EPTansuo <eptansuo@163.com>
2024-12-04 12:26:15 +0800

    support mrom and sram

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -6,6 +6,7 @@ extern char _heap_start;
 int main(const char *args);
 
 extern char _pmem_start;
+
 #define PMEM_SIZE (8 * 1024 * 1024)
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
@@ -15,9 +16,26 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 #endif
 static const char mainargs[] = MAINARGS;
 
+void UART_send(uint8_t c);
 void putch(char ch) {
-  outb(0x10000000, ch);
+  UART_send(ch);
 }
+
+extern char _ram_data_start, _data_start, _data_end, _bss_start, _bss_end;
+
+void bootloader(){
+  char *src = &_ram_data_start;
+  char *dst = &_data_start;
+  while(dst < & _data_end) {
+    *dst++ = *src++;
+  }
+
+  char *p = &_bss_start;
+  while(p < &_bss_end) {
+    *p++ = 0;
+  }
+}
+
 
 void halt(int code) {
   asm volatile("ebreak");
@@ -25,6 +43,7 @@ void halt(int code) {
 }
 
 void _trm_init() {
+  bootloader();
   int ret = main(mainargs);
   halt(ret);
 }
