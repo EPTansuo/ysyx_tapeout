168ff0e7cc7c949421c2f24091de1969150dcc56
tracer-ysyx <tracer@ysyx.org>
2024-12-04 19:34:06 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  19:34:06 up 6 days,  3:53,  2 users,  load average: 2.58, 2.07, 2.41

--- nemu/include/cpu/ifetch.h
+++ nemu/include/cpu/ifetch.h
@@ -24,7 +24,7 @@ uint8_t *guest_to_host_mrom(paddr_t paddr);
 static inline uint32_t inst_fetch(vaddr_t *pc, int len) {
 #ifdef CONFIG_TARGET_SHARE
   uint32_t inst = *guest_to_host_mrom(*pc);
-  printf("inst_fetch: pc = 0x%lx, inst = 0x%x\n", *pc, inst);
+  printf("inst_fetch: pc = 0x%x, inst = 0x%x\n", *pc, inst);
 #else // !CONFIG_TARGET_SHARE
   uint32_t inst = vaddr_ifetch(*pc, len);
 #endif 
