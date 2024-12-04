d0c27b5e54d73f204d0076d61ada06c560d9401b
tracer-ysyx <tracer@ysyx.org>
2024-12-04 11:46:55 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  11:46:55 up 5 days, 20:06,  2 users,  load average: 1.06, 1.76, 1.84

--- abstract-machine/am/src/riscv/ysyxsoc/uart.c
+++ abstract-machine/am/src/riscv/ysyxsoc/uart.c
@@ -36,6 +36,8 @@ void UART_send(uint8_t c) {
     outb(UART_BASE + UART_FC,0xc0);    // send
     outb(UART_BASE + UART_FC,0xc0);    // send
     outb(UART_BASE + UART_FC,0xc0);    // send
+    outb(UART_BASE + UART_FC,0xc0);    // send
+    outb(UART_BASE + UART_FC,0xc0);    // send
     if(line_status & 0x02) {
 
         outb(UART_BASE + UART_FC, 0x06); // Clear all FIFOs
