d4533cdcd7cf5666fccc60b5ab4fac5d88353b4d
tracer-ysyx <tracer@ysyx.org>
2024-12-04 14:22:50 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  14:22:50 up 5 days, 22:42,  2 users,  load average: 2.51, 2.67, 2.73

--- npc/csrc/cpu/dpi-c/dpi-c.c
+++ npc/csrc/cpu/dpi-c/dpi-c.c
@@ -167,6 +167,7 @@ end_pmem_write:
 extern "C" void flash_read(int32_t addr, int32_t *data) { 
 
  *data = addr ;
+ printf("read flash addr = %x\n", addr);
 
 }
 extern "C" void mrom_read(int32_t addr, int32_t *data) { 
