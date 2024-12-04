d3be08fcfe6379bf93e37fc670df1fc8465bca1a
tracer-ysyx <tracer@ysyx.org>
2024-12-04 20:00:43 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  20:00:43 up 6 days,  4:20,  2 users,  load average: 1.35, 0.75, 1.12

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -36,10 +36,20 @@ word_t vaddr_read(vaddr_t addr, int len) {
 #ifndef CONFIG_TARGET_SHARE 
   return paddr_read(addr, len);
 #else
+  word_t result = 0;
   if(addr < 0x20000000)
     return paddr_read(addr, len);
-  else
-    return *(word_t *)guest_to_host_mrom(addr);
+  else{
+    if(len == 1)
+      result = *(uint8_t *)guest_to_host_mrom(addr);
+    else if(len == 2)
+      result = *(uint16_t *)guest_to_host_mrom(addr);
+    else if(len == 4)
+      result = *(uint32_t *)guest_to_host_mrom(addr);
+    else
+      assert(0);
+  }
+  return result;
 #endif 
 }
 
