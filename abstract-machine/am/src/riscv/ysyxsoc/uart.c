eb80cf850477e36fdff22ce66d4954117ca7bbae
tracer-ysyx <tracer@ysyx.org>
2024-12-05 21:31:55 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  21:31:55 up 7 days,  5:51,  2 users,  load average: 5.68, 5.57, 5.91

--- abstract-machine/am/src/riscv/ysyxsoc/uart.c
+++ abstract-machine/am/src/riscv/ysyxsoc/uart.c
@@ -22,7 +22,7 @@
  // NVBoard中，Update 一次(1 clk)，divisor_cnt就减1，传输一个比特, (divisor_cnt+1默认等于16)
  // 也就是 ---> 1-bit (divisor_cnt+1) cycles, 所以最少设置为32
  // 该串口 ---> 1-bit (32*DIVISOR) cycles
-#define DIVISOR 1
+#define DIVISOR 2
 
 void __am_uart_init() {
     UART_LC |= 0x80;
