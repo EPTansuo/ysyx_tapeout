f41d9e1fa548b2a7fa5528b45400f3868e3ccd93
tracer-ysyx <tracer@ysyx.org>
2024-12-05 16:32:13 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  16:32:13 up 7 days, 51 min,  2 users,  load average: 3.54, 1.90, 1.29

--- npc/src/main/scala/lsu.scala
+++ npc/src/main/scala/lsu.scala
@@ -61,7 +61,7 @@ class LSU(xlen: Int) extends Module {
     val dmem_rdata = RegInit(0.U(xlen.W))
     val roffset = alu_out(1, 0) << 3.U 
     when(io.dmem.r.valid){
-        dmem_rdata := dmem_rdata_tmp// << roffset
+        dmem_rdata := dmem_rdata_tmp >> roffset
     }
 
     val ld_data = MuxLookup(ctrlsig.ld_sel, 0.U(xlen.W))(Seq(
