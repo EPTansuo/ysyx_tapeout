1b8bea958880b7788118f7588b420b2611c09233
tracer-ysyx <tracer@ysyx.org>
2024-12-05 20:52:06 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  20:52:06 up 7 days,  5:11,  2 users,  load average: 9.92, 8.41, 7.06

--- abstract-machine/am/src/riscv/ysyxsoc/uart.c
+++ abstract-machine/am/src/riscv/ysyxsoc/uart.c
@@ -19,8 +19,10 @@
 #define UART_DVLSB (*(volatile uint8_t *)(UART_BASE + 0)) // RW  Divisor Latch 1(LSB)
 #define UART_DVMSB (*(volatile uint8_t *)(UART_BASE + 1)) // RW  Divisor Latch 2(MSB)
 
-// #define DIVISOR (uint16_t)(12000000 / (16 * 9600))
-#define DIVISOR 1
+ // NVBoard中，Update一次，divisor_cnt（默认为15） 就减1，传输一个比特, 也就是波特率为 clk_freq / (8 * （divisor_cnt+1）)
+ // 该串口的波特率为： clk_freq / (16 * divisor) 
+ // 故此处divisor应该为 8
+#define DIVISOR 8
 
 void __am_uart_init() {
     UART_LC |= 0x80;
