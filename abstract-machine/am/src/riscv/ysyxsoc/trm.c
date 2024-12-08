c5c1336416259cfbe54f522e345c9c293d027b53
EPTansuo <eptansuo@163.com>
2024-12-08 13:19:34 +0800

    tmp

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -41,6 +41,11 @@ void bootloader(){
   }
 }
 
+// void __attribute__((section(".fsbl"))) _fsbl_init(){
+
+// }
+
+
 void print_stuID(){
   uint32_t mvendorid, marchid;
   asm volatile (
@@ -70,7 +75,7 @@ void UART_init();
 void _trm_init() {
   bootloader();
   ioe_init();
-  //print_stuID();
+  print_stuID();
   int ret = main(mainargs);
   halt(ret);
 }
