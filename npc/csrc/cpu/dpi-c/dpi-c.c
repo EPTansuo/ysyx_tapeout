e142795755a9b8f4982e343d2d9545aba014e516
tracer-ysyx <tracer@ysyx.org>
2024-12-12 21:09:35 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  21:09:35 up 11:25,  2 users,  load average: 1.37, 1.19, 0.89

--- npc/csrc/cpu/dpi-c/dpi-c.c
+++ npc/csrc/cpu/dpi-c/dpi-c.c
@@ -214,8 +214,8 @@ void sdram_write(input byte bank, input int row, input int col,
 */
 
 extern "C" int sdram_read(char bank, int row, int col, char block_num) { 
-  printf("sdram_read bank = %d, row = %d, col = %d, block_num = %d, data = %x\n", bank, row, col, 
-                block_num, sdram[block_num][bank][row][col]);
+//  printf("sdram_read bank = %d, row = %d, col = %d, block_num = %d, data = %x\n", bank, row, col, 
+//                block_num, sdram[block_num][bank][row][col]);
   return sdram[block_num][bank][row][col];
 }
 
@@ -232,7 +232,7 @@ extern "C" void sdram_write(char bank, int row, int col , int wdata, char wmask,
     }
   }
 
-  printf("sdram_write bank = %d, row = %d, col = %d, block_num = %d, wdata = %x, wmask = %x\n", bank, row, col,
-                 block_num, wdata, wmask);
+//  printf("sdram_write bank = %d, row = %d, col = %d, block_num = %d, wdata = %x, wmask = %x\n", bank, row, col,
+//                 block_num, wdata, wmask);
 }
 
