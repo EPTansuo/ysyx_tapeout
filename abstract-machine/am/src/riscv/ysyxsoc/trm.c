d353f20cc1784f0da650138171be94624683bb14
tracer-ysyx <tracer@ysyx.org>
2024-12-12 23:06:59 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  23:06:59 up 13:23,  2 users,  load average: 2.01, 2.37, 2.05

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -49,14 +49,11 @@ void __attribute__((section(".bootloader"))) bootloader(){
 extern char _bootloader_src, _siflash_ssbl, _eiflash_ssbl;
 
 void __attribute__((section(".fsbl"))) _fsbl_init(){
-  putch('a');
   char *src = &_bootloader_src;   // LMA, flash
   char *dst = &_siflash_ssbl;    // VMA, psram
   while(dst < (&_eiflash_ssbl)) { 
-    putch('c');
     *dst++ = *src++;
   }
-  putch('b');
   bootloader();
 }
 
