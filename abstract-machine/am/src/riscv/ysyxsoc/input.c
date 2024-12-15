03c6ddffcc31730f36f1dd67dbe613220dee2a02
tracer-ysyx <tracer@ysyx.org>
2024-12-15 14:47:33 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  14:47:33 up 3 days,  5:03,  2 users,  load average: 3.36, 3.18, 3.49

--- abstract-machine/am/src/riscv/ysyxsoc/input.c
+++ abstract-machine/am/src/riscv/ysyxsoc/input.c
@@ -1,5 +1,8 @@
 #include <am.h>
 
+#define PS2_BASE 0x10011000
+#define PS2_KEY (*(volatile uint8_t*)(PS2_BASE))
+
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   kbd->keydown = 0;
   kbd->keycode = AM_KEY_NONE;
