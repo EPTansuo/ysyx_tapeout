e1b15aafd30aeb3f55caad881adf85d78d7f3240
EPTansuo <eptansuo@163.com>
2024-12-16 16:15:12 +0800

    finish SDRAM

--- npc/csrc/cpu/dpi-c/dpi-c.c
+++ npc/csrc/cpu/dpi-c/dpi-c.c
@@ -233,6 +233,6 @@ extern "C" void sdram_write(char bank, int row, int col , int wdata, char wmask,
   }
 
   //printf("sdram_write bank = %d, row = %d, col = %d, block_num = %d, wdata = %x, wmask = %x\n", bank, row, col,
-                 block_num, wdata, wmask);
+  //               block_num, wdata, wmask);
 }
 
