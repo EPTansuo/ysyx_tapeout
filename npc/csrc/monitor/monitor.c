73138d4da08a550e44dabfadcbe8f025d0257a5a
tracer-ysyx <tracer@ysyx.org>
2024-12-17 10:51:00 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  10:51:00 up 5 days,  1:07,  2 users,  load average: 7.28, 7.59, 6.31

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -67,7 +67,7 @@ const char *get_img_file(){
 
 static int parse_args(int argc, char *argv[]) {
   for(int i=0; i<argc; i++){
-      printf("argv[%d]: %s\n", i, argv[i]);
+      Log("argv[%d]: %s", i, argv[i]);
   }
   
   const struct option table[] = {
