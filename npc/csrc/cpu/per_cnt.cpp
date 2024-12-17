4567b9f546d53f6e44bf529d3090906dcb9e3a44
tracer-ysyx <tracer@ysyx.org>
2024-12-17 17:41:10 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  17:41:10 up 5 days,  7:57,  2 users,  load average: 6.86, 6.81, 6.43

--- npc/csrc/cpu/per_cnt.cpp
+++ npc/csrc/cpu/per_cnt.cpp
@@ -65,8 +65,8 @@ void perf_statistic(){
     printf(L_BLUE);
     PRINT_PERF("Totcal Insts", inst_cnt, (double)inst_cnt/cycle_cnt, "IPC");
     printf(COLOR_NONE);
-    printf("\n---------------------------------- Inst Withdraw ---------------------------------\n");
-    PRINT_PERF("Inst Withdraw CNT", ifu_cnt, (double)ifu_cnt/inst_cnt*100, "% per inst");
+    printf("\n----------------------------------- Inst Fetch -----------------------------------\n");
+    PRINT_PERF("Inst Fetch CNT", ifu_cnt, (double)ifu_cnt/inst_cnt*100, "% per inst");
     printf("\n----------------------------------- Inst Type ------------------------------------\n");
     PRINT_PERF("Inst Compute  CNT", inst_compute_cnt, (double)inst_compute_cnt/inst_cnt*100, "% per inst");
     PRINT_PERF("Inst Branch   CNT", inst_branch_cnt, (double)inst_branch_cnt/inst_cnt*100, "% per inst");
