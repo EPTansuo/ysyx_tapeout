20e0dc5a26d8e2b81656b4887e72a506b39b6cfa
tracer-ysyx <tracer@ysyx.org>
2024-12-17 17:26:32 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  17:26:32 up 5 days,  7:42,  2 users,  load average: 6.10, 5.90, 5.56

--- npc/csrc/cpu/per_cnt.cpp
+++ npc/csrc/cpu/per_cnt.cpp
@@ -52,7 +52,7 @@ void perf_get_data(){
 }
 
 #define PRINT_PERF(name, cnt, process_cnt, unit) \
-    printf("%28s: %'14lu         # %6.4lf %s\n", \
+    printf("%28s: %'14lu         # %10.4lf %s\n", \
            name, (uint64_t)cnt, process_cnt, unit);
 
 void perf_statistic(){
