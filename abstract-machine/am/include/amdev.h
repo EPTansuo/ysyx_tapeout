6f6ed12e6c936449b3884ddffad2ebfe986fab67
tracer-ysyx <tracer@ysyx.org>
2024-12-17 12:17:08 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  12:17:07 up 5 days,  2:33,  2 users,  load average: 9.44, 7.94, 6.84

--- abstract-machine/am/include/amdev.h
+++ abstract-machine/am/include/amdev.h
@@ -33,7 +33,6 @@ AM_DEVREG(23, NET_TX,       WR, Area buf);
 AM_DEVREG(24, NET_RX,       WR, Area buf);
 
 // Input
-
 #define AM_KEYS(_) \
   _(ESCAPE) _(F1) _(F2) _(F3) _(F4) _(F5) _(F6) _(F7) _(F8) _(F9) _(F10) _(F11) _(F12) \
   _(GRAVE) _(1) _(2) _(3) _(4) _(5) _(6) _(7) _(8) _(9) _(0) _(MINUS) _(EQUALS) _(BACKSPACE) \
@@ -41,17 +40,22 @@ AM_DEVREG(24, NET_RX,       WR, Area buf);
   _(CAPSLOCK) _(A) _(S) _(D) _(F) _(G) _(H) _(J) _(K) _(L) _(SEMICOLON) _(APOSTROPHE) _(RETURN) \
   _(LSHIFT) _(Z) _(X) _(C) _(V) _(B) _(N) _(M) _(COMMA) _(PERIOD) _(SLASH) _(RSHIFT) \
   _(LCTRL) _(APPLICATION) _(LALT) _(SPACE) _(RALT) _(RCTRL) \
-  _(UP) _(DOWN) _(LEFT) _(RIGHT) _(INSERT) _(DELETE) _(HOME) _(END) _(PAGEUP) _(PAGEDOWN) \
-    \
+  _(UP) _(DOWN) _(LEFT) _(RIGHT) _(INSERT) _(DELETE) _(HOME) _(END) _(PAGEUP) _(PAGEDOWN)
+
+#define AM_KEYS_EXT(_) \
   _(CAPS) _(ESC) _(SCROLL) _(NUM) _(KPSTAR) _(KPMINUS) _(KPPLUS) _(KP) _(KP0) _(KP1) _(KP2) _(KP3) \
   _(KP4) _(KP5) _(KP6) _(KP7) _(KP8) _(KP9) _(KPENTER) _(KPSLASH) _(PRINTSCRN) _(PAUSE) _(LGUI) _(RGUI) \
   _(ENTER) 
  
 
 #define AM_KEY_NAMES(key) AM_KEY_##key,
+#define AM_KEY_EXT_NAMES(key) AM_KEY_##key,
+
+
 enum {
   AM_KEY_NONE = 0,
   AM_KEYS(AM_KEY_NAMES)
+  AM_KEYS_EXT(AM_KEY_EXT_NAMES)
 };
 
 // GPU
