69f7c56481d43aed2049f3bb69ed92084ddaf450
tracer-ysyx <tracer@ysyx.org>
2024-12-19 21:29:47 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  21:29:47 up 7 days, 11:45,  2 users,  load average: 7.58, 6.49, 5.56

--- npc/csrc/utils/pc_trace.c
+++ npc/csrc/utils/pc_trace.c
@@ -9,9 +9,13 @@ MUXDEF(CONFIG_PC_TRACE_COMPRESS, gzFile, FILE*) pc_trace_fp;)
 
 void init_pc_trace(){
   char buf[100];
-  sprintf(buf, "%s/build/pc_trace.txt", getenv("NPC_HOME"));
+  MUXDEF(CONFIG_PC_TRACE_BINARY,
+  sprintf(buf, "%s/build/pc_trace.bin", getenv("NPC_HOME")),
+  sprintf(buf, "%s/build/pc_trace.txt", getenv("NPC_HOME")));
+
 #ifdef CONFIG_PC_TRACE_COMPRESS
-  pc_trace_fp = log_open(buf ".gz");
+  strcat(buf, ".gz");
+  pc_trace_fp = gzopen(buf, "wb");
 #else 
   pc_trace_fp = fopen(buf, "wb");
 #endif 
@@ -19,14 +23,18 @@ void init_pc_trace(){
 
 void pc_trace(word_t pc){
 #ifdef CONFIG_PC_TRACE_COMPRESS
-  log_printf(pc_trace_fp, "%lx\n", pc); 
+  MUXDEF(CONFIG_PC_TRACE_BINARY, 
+  gzwrite(pc_trace_fp, (char*)&pc, sizeof(word_t)),
+  log_printf(pc_trace_fp, "%lx\n", pc));
 #else  
-  fprintf(pc_trace_fp, FMT_WORD_HEX "\n", pc);
+  MUXDEF(CONFIG_PC_TRACE_BINARY,
+  fwrite(&pc, sizeof(word_t), 1, pc_trace_fp),
+  fprintf(pc_trace_fp, FMT_WORD_HEX "\n", pc));
 #endif
 }
 void pc_trace_close(){
 #ifdef CONFIG_PC_TRACE_COMPRESS
-  log_close(pc_trace_fp);
+  gzclose(pc_trace_fp);
 #else 
   fclose(pc_trace_fp);
 #endif
