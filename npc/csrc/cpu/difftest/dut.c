2027212d6e3e7825b607269c30793bb5858d3601
tracer-ysyx <tracer@ysyx.org>
2024-12-20 13:20:02 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  13:20:02 up 8 days,  3:36,  2 users,  load average: 6.35, 7.34, 6.91

--- npc/csrc/cpu/difftest/dut.c
+++ npc/csrc/cpu/difftest/dut.c
@@ -89,11 +89,11 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 
   ref_difftest_init(port);
   ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
-  npc_cpu.pc = RESET_VECTOR;
+  npc_cpu.pc = 0x30000000;
   npc_cpu.csr.mstatus = MUXDEF(CONFIG_RV64, 0xa00001800, 0x1800);
   npc_cpu.csr.mtvec = MUXDEF(CONFIG_RV64, 0x80000000, 0x100);
   npc_cpu.csr.mepc = 0;
-  ref_difftest_regcpy(&npc_cpu, DIFFTEST_TO_REF);
+  //ref_difftest_regcpy(&npc_cpu, DIFFTEST_TO_REF);
 }
 
 static void checkregs(CPU_state *ref, vaddr_t pc) {
