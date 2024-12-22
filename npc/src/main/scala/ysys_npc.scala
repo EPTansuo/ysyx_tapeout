a90edd9a654b5111f82fb9bd620aa83b62ca374c
tracer-ysyx <tracer@ysyx.org>
2024-12-22 19:35:57 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  19:35:57 up 10 days,  9:52,  3 users,  load average: 5.09, 5.34, 4.90

--- npc/src/main/scala/ysys_npc.scala
+++ npc/src/main/scala/ysys_npc.scala
@@ -41,12 +41,12 @@ class ysyx_npc(xlen:Int) extends Module {
     exu.io.reg_read2 <> regfile.io.read2
     wbu.io.reg_write <> regfile.io.write
 
-    //val icache = Module(new ICache(ICacheParameters(), AXI4BundleParameters(xlen, 32, AXI_IDBITS)))
-    //ifu.io.imem <> icache.io.ifu 
+    val icache = Module(new ICache(ICacheParameters(), AXI4BundleParameters(xlen, 32, AXI_IDBITS)))
+    ifu.io.imem <> icache.io.ifu 
 
     val axi_arbiter = Module( new AXIArbiter(2, new AXI4BundleParameters(xlen, 32, AXI_IDBITS)))
-    //axi_arbiter.io.in(0) <> icache.io.imem
-    axi_arbiter.io.in(0) <> ifu.io.imem
+    axi_arbiter.io.in(0) <> icache.io.imem
+    //axi_arbiter.io.in(0) <> ifu.io.imem
     axi_arbiter.io.in(1) <> lsu.io.dmem
     axi_arbiter.io.out <> io.axi
 
