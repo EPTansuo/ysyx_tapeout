17e7bc08e4e523c09d54b87ed2a95a07f825a68a
tracer-ysyx <tracer@ysyx.org>
2024-12-23 19:43:42 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  19:43:42 up 52 min,  2 users,  load average: 1.45, 1.01, 0.90

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,15 +1,15 @@
 #include <common.h>
 
-
+Context* schedule(Context *prev);
 void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    
-    case EVENT_YIELD : printf("Yield Event!\n"); /*c->mepc += 4;*/ break;
-    case EVENT_SYSCALL : printf("Syscall Event!\n"); c->mepc += 4; do_syscall(c); break;
+    case EVENT_YIELD: case EVENT_IRQ_TIMER: 
+      c = schedule(c); break;
+    case EVENT_SYSCALL: 
+      do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
-
   return c;
 }
 
