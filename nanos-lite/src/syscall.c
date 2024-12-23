cedb8efc55663d4b454b3c9b6c13b66dcbc7dd34
tracer-ysyx <tracer@ysyx.org>
2024-12-23 20:32:11 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  20:32:11 up  1:41,  2 users,  load average: 2.38, 2.35, 1.63

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,12 +6,17 @@ void do_syscall(Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    case SYS_yield: c->GPRx = 0; yield(); break;
-    case SYS_time: 
-    uint64_t time = io_read(AM_TIMER_UPTIME).us;
-    ((struct timeval *)a[1])->tv_sec = time / 1000000;
-    ((struct timeval *)a[1])->tv_usec = time % 1000000;
-    c->GPRx = 0;
+    case SYS_yield: {
+      c->GPRx = 0; yield(); 
+      break;
+    }
+    case SYS_time: {
+      uint64_t time = io_read(AM_TIMER_UPTIME).us;
+      ((struct timeval *)a[1])->tv_sec = time / 1000000;
+      ((struct timeval *)a[1])->tv_usec = time % 1000000;
+      c->GPRx = 0;
+      break;
+    }
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
