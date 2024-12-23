7ef2b800546487549bd9ed4de86abebca6c33252
tracer-ysyx <tracer@ysyx.org>
2024-12-23 20:52:53 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  20:52:53 up  2:02,  2 users,  load average: 2.20, 1.11, 1.18

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,13 +1,29 @@
 #include <common.h>
 #include "syscall.h"
 #include <sys/time.h>
+
+#ifdef STRACE
+const char *syscalls[] = {"SYS_exit", "SYS_yield", "SYS_open",
+"SYS_read", "SYS_write", "SYS_kill", "SYS_getpid", "SYS_close", 
+"SYS_lseek", "SYS_brk", "SYS_fstat", "SYS_time", "SYS_signal", 
+"SYS_execve", "SYS_fork", "SYS_link", "SYS_unlink", "SYS_wait",
+"SYS_times", "SYS_gettimeofday"};
+#endif 
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
+
+#ifdef STRACE
+Log("SYSCALL: %s", syscalls[a[0]]);
+#endif 
+
+
   switch (a[0]) {
     case SYS_exit: {
-      halt(0);
+      //halt(0);
+
       break;
     }
     case SYS_yield: {
