416d7a4e2a6a865e2af53520fd790be2ef1a013c
tracer-ysyx <tracer@ysyx.org>
2024-12-24 22:13:09 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  22:13:09 up  1:47,  2 users,  load average: 0.87, 0.82, 0.89

--- npc/src/main/scala/ifu.scala
+++ npc/src/main/scala/ifu.scala
@@ -31,7 +31,7 @@ class IFU(config: NPCConfig) extends Module {
   val state = RegInit(s_idle)         
   state := MuxLookup(state, s_idle)(Seq(
     s_idle -> Mux(in_valid, s_read, s_idle),
-    s_read -> Mux(io.imem.ar.ready, s_wait_read, s_read),
+    s_read -> Mux(io.imem.ar.ready, Mux(io.imem.r.valid, s_wait_ready, s_wait_read), s_read),
     s_wait_read -> Mux(io.imem.r.valid, s_wait_ready, s_wait_read),
     s_wait_ready -> Mux(io.out.ready, s_idle, s_wait_ready)
   ))
