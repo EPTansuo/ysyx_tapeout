1b87c2985c3bfb7de443e5e5875a0ee2a13d4aa9
tracer-ysyx <tracer@ysyx.org>
2024-12-24 13:54:48 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  13:54:48 up 19:04,  2 users,  load average: 2.71, 2.18, 2.02

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -127,7 +127,7 @@ typedef struct {
   uint32_t type;
 } ICacheEntry;
 
-ICacheEntry  icache[ICACHE_SIZE] PG_ALIGN =  {};
+ICacheEntry  icache[ICACHE_SIZE] PG_ALIGN =  {0};
 uint64_t icache_hit = 0;
 uint64_t icache_miss = 0;
 
@@ -151,9 +151,8 @@ static int decode_exec(Decode *s) {
   icache[index].type = TYPE_##t; \
 }
 
-  uint32_t  inst = INSTPAT_INST(s);
   unsigned index = (s->pc) % ICACHE_SIZE;
-  if (icache[index].inst == inst ) {
+  if (icache[index].inst == s->isa.inst.val ) {
         if(icache[index].label != NULL){
           decode_operand(s, &rd, &src1, &src2, &imm, icache[index].type);
           icache_hit++;
