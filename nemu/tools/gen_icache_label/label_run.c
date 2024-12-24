b74a67287031c57ff651dd8dfbd3b95febe58c37
tracer-ysyx <tracer@ysyx.org>
2024-12-24 16:01:16 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  16:01:16 up 21:10,  2 users,  load average: 1.81, 1.92, 2.33

--- nemu/tools/gen_icache_label/label_run.c
+++ nemu/tools/gen_icache_label/label_run.c
@@ -64,3 +64,4 @@ exe_ebreak:  rd = icache[index].rd; NEMUTRAP(s->pc, R(10)); goto __instpat_end_;
 exe_inv   :  rd = icache[index].rd; INV(s->pc); goto __instpat_end_;
 exe_slliw :  src1 = R(icache[index].rs1); imm = icache[index].imm;rd = icache[index].rd; R(rd) = SEXT((uint32_t)src1 << SHAMT, 32); goto __instpat_end_;
 exe_srliw :  src1 = R(icache[index].rs1); imm = icache[index].imm;rd = icache[index].rd; R(rd) = SEXT((uint32_t)src1 >> SHAMT, 32); goto __instpat_end_;
+exe_lwu   :  src1 = R(icache[index].rs1); imm = icache[index].imm;rd = icache[index].rd; R(rd) = Mr(src1 + imm, 4); goto __instpat_end_;
