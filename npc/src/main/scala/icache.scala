30395657d49f8e48e132a0a37231a9a86ceafb0c
tracer-ysyx <tracer@ysyx.org>
2024-12-31 18:29:34 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  18:29:34 up 1 day,  1:10,  2 users,  load average: 3.49, 3.32, 2.97

--- npc/src/main/scala/icache.scala
+++ npc/src/main/scala/icache.scala
@@ -103,6 +103,7 @@ class ICache(config: NPCConfig) extends Module{
     }
     
     val burst_cnt = RegInit(0.U(log2Ceil(blockSize/4).W))
+    val imem_first_read = 
     //bypass := 1.U 
     val s_idle :: s_read :: s_replace :: s_refill :: s_wait :: Nil = Enum(5)
 
@@ -110,8 +111,8 @@ class ICache(config: NPCConfig) extends Module{
     val state_next = Wire(UInt(state.getWidth.W))
     state_next := MuxLookup(state, s_idle)(Seq(
         s_idle -> Mux(io.ifu.ar.valid, Mux(bypass.asBool, s_idle, s_read), s_idle),
-        s_read -> Mux(hit && burst_cnt === 0.U, s_idle, s_replace),
-        s_replace -> Mux(io.imem.ar.ready, s_refill, s_replace),
+        s_read -> Mux(hit && burst_cnt === 0.U, s_idle,Mux(burst_cnt =/= 0.U, s_refill, s_replace)),
+        s_replace -> Mux(io.imem.ar.ready  s_refill, s_replace),
         s_refill -> Mux(io.imem.r.valid, Mux(burst_cnt === (blockSize/4-1).U, s_wait, s_read), s_refill),
         s_wait -> s_read,
     ))
@@ -131,8 +132,8 @@ class ICache(config: NPCConfig) extends Module{
     dontTouch(io.ifu.r.valid)
 
     io.imem.ar.bits.addr := io.ifu.ar.bits.addr
-    io.imem.ar.valid := Mux(bypass.asBool, io.ifu.ar.valid, (state === s_replace))
-    io.imem.r.ready := Mux(bypass.asBool, io.ifu.r.valid ,state === s_refill)
+    io.imem.ar.valid := Mux(bypass.asBool, io.ifu.ar.valid, (state === s_replace && burst_cnt === 0.U))
+    io.imem.r.ready := Mux(bypass.asBool, io.ifu.r.ready ,state === s_refill)
 
 
     
