2002d389ce264fce82ddb07f75a6b19e2861f181
tracer-ysyx <tracer@ysyx.org>
2024-12-31 15:47:03 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  15:47:03 up 22:28,  2 users,  load average: 2.56, 1.71, 1.02

--- npc/src/main/scala/lsu.scala
+++ npc/src/main/scala/lsu.scala
@@ -122,7 +122,7 @@ val s_idle :: s_exe :: s_read :: s_wait_read :: s_read_2 :: s_wait_read_2 :: s_w
     io.dmem.ar.valid := state === s_read || state === s_read_2
     io.dmem.ar.bits.addr := Mux(state === s_read_2 || state === s_wait_read_2, alu_out + 4.U, alu_out);
     io.dmem.ar.bits.prot := 0.U
-    io.dmem.r.ready := state === s_read || state === s_read_2
+    io.dmem.r.ready := state === s_read || state === s_read_2 || state === s_wait_read || state === s_wait_read_2
     
 
     io.dmem.ar.bits.id := 0.U
