befdf8e0acf55999aa8bbb9f6f952a8e49c8f020
tracer-ysyx <tracer@ysyx.org>
2025-01-01 14:35:07 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  14:35:07 up  2:11,  2 users,  load average: 3.36, 3.20, 3.10

--- npc/src/main/scala/ifu.scala
+++ npc/src/main/scala/ifu.scala
@@ -23,7 +23,8 @@ class IFU(config: NPCConfig) extends Module {
   when(isFirst){
     isFirst := false.B
   }
-  val in_valid = Mux(isFirst, true.B, io.in.valid)
+  //val in_valid = Mux(isFirst, true.B, io.in.valid)
+  val in_valid = true.B
   val in_ready = io.in.ready
 
 
