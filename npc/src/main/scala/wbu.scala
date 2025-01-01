13844e7987222515ee5949d2dcc833001c122139
tracer-ysyx <tracer@ysyx.org>
2025-01-01 13:36:06 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  13:36:06 up  1:12,  2 users,  load average: 3.06, 2.84, 2.74

--- npc/src/main/scala/wbu.scala
+++ npc/src/main/scala/wbu.scala
@@ -16,27 +16,27 @@ class WBU(config: NPCConfig) extends Module {
         val reg_write = Flipped(new RegfileWriteIO(config.XLEN))
     })
 
-    val in_reg = Reg(Output(chiselTypeOf(io.in)))
-    when( io.in.valid && io.in.ready){
-        in_reg := io.in
-    }
-    val pc = in_reg.bits.pc
-    val ctrlsig = in_reg.bits.wbu
-    val src1 = in_reg.bits.src1
-    val alu_out = in_reg.bits.alu_out
-    val ld_data = in_reg.bits.ld_data
-    val rd_addr = in_reg.bits.rd_addr
-    val npc = in_reg.bits.npc
-    val csr_out = in_reg.bits.csr_out
+    // val in_reg = Reg(Output(chiselTypeOf(io.in)))
+    // when( io.in.valid && io.in.ready){
+    //     in_reg := io.in
+    // }
+    // val pc = in_reg.bits.pc
+    // val ctrlsig = in_reg.bits.wbu
+    // val src1 = in_reg.bits.src1
+    // val alu_out = in_reg.bits.alu_out
+    // val ld_data = in_reg.bits.ld_data
+    // val rd_addr = in_reg.bits.rd_addr
+    // val npc = in_reg.bits.npc
+    // val csr_out = in_reg.bits.csr_out
 
-    // val pc = io.in.bits.pc
-    // val ctrlsig = io.in.bits.wbu
-    // val src1 = io.in.bits.src1
-    // val alu_out = io.in.bits.alu_out
-    // val ld_data = io.in.bits.ld_data
-    // val rd_addr = io.in.bits.rd_addr
-    // val npc = io.in.bits.npc
-    // val csr_out = io.in.bits.csr_out
+    val pc = io.in.bits.pc
+    val ctrlsig = io.in.bits.wbu
+    val src1 = io.in.bits.src1
+    val alu_out = io.in.bits.alu_out
+    val ld_data = io.in.bits.ld_data
+    val rd_addr = io.in.bits.rd_addr
+    val npc = io.in.bits.npc
+    val csr_out = io.in.bits.csr_out
 
     val s_idle :: s_wait_ready :: Nil = Enum(2)
 
