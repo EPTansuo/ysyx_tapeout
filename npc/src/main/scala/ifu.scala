555628ce197fcd0e8afe33a89fbf3b781dacfc2b
tracer-ysyx <tracer@ysyx.org>
2025-01-01 14:50:32 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.60-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 16:03:02 +0000 x86_64 GNU/Linux  14:50:32 up  2:26,  2 users,  load average: 3.44, 3.50, 3.57

--- npc/src/main/scala/ifu.scala
+++ npc/src/main/scala/ifu.scala
@@ -42,12 +42,12 @@ class IFU(config: NPCConfig) extends Module {
   io.in.ready := state === s_idle
 
   val pc = RegInit(config.PC_INIT.U)
-  // when( io.in.valid && io.in.ready){
-  //     pc := io.in.bits.npc
-  // }
-  when(io.out.valid){
-    pc := pc + 4.U
+  when( io.in.valid && io.in.ready){
+      pc := io.in.bits.npc
   }
+  // when(io.out.valid){
+  //   pc := pc + 4.U
+  // }
 
   io.imem.ar.valid := state === s_read
   io.imem.ar.bits.addr := pc
