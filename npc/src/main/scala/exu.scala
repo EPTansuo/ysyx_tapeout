505d7bff07264373e06c5acf47721dd686220233
tracer-ysyx <tracer@ysyx.org>
2025-01-03 21:44:12 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  21:44:12 up  1:24,  1 user,  load average: 1.66, 1.15, 1.27

--- npc/src/main/scala/exu.scala
+++ npc/src/main/scala/exu.scala
@@ -22,9 +22,7 @@ class EXU(config: NPCConfig) extends Module{
         val reg_read2 = Flipped(new RegfileReadIO(config.XLEN))
        // val flush = Input(Bool())
         val npc = Decoupled(UInt(config.XLEN.W))
-        //val forward = Decoupled(new SigIO_FORWARD(config.XLEN))
-        val rd_addr = Output(UInt(5.W))
-        val stall = Input(Bool())
+        // val forward = Decoupled(UInt(config.XLEN.W))
     })
     val xlen = config.XLEN
     val alu = Module(new ALU(xlen))
@@ -68,30 +66,6 @@ class EXU(config: NPCConfig) extends Module{
     val rd_addr = inst(11, 7)
     val rs1_addr = inst(19, 15)
     val rs2_addr = inst(24, 20)
-    val rd_addr_reg = RegInit(0.U(5.W))
-    
-
-    io.rd_addr := rd_addr_reg
-    when(io.stall){
-        rd_addr_reg := 0.U
-    }.otherwise{
-        rd_addr_reg := rd_addr
-    }
-    // // Forwarding
-    // forward.bits.rs1 := rs1_addr
-    // forward.bits.rs2 := rs2_addr
-    // forward.bits.rd := rd_addr
-    // forward.bits.rd_data := MuxLookup(wb_sel, 0.U(xlen.W))(Seq(
-    //     WB_ALU -> alu.io.out,
-    //     //WB_MEM -> io.in.bits.lsu.ld_data,
-    //     WB_PC4 -> (pc + 4.U),
-    //     //WB_CSR -> io.out.bits.csr_out
-    //     )
-    // )
-    // forward.bits.valid := wb_sle === WB_ALU || wb_sel === WB_MEM && (state === s_exe || state === s_wait_ready)
-
-    
-    
 
 
     
@@ -157,6 +131,11 @@ class EXU(config: NPCConfig) extends Module{
     io.npc.bits := npc 
     io.npc.valid := state === s_exe
 
+
+
+    // // Forwarding
+    // io.forward.bits := alu.io.out // TODO: csrrw
+    // io.forward.valid := state === s_exe && ctrlsig.ld_sel =/= LD_XX && ctrlsig.wb_sel =/= WB_XX
 
 
     io.out.bits.csr_out  := csr.io.out
