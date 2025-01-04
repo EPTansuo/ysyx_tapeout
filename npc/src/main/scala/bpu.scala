deac4ab0106aaa0ac2fe94b4331d55577b646b1c
tracer-ysyx <tracer@ysyx.org>
2025-01-04 14:12:36 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  14:12:36 up  4:09,  1 user,  load average: 1.80, 1.23, 1.19

--- npc/src/main/scala/bpu.scala
+++ npc/src/main/scala/bpu.scala
@@ -7,10 +7,10 @@ class BPU(config:NPCConfig) extends Module{
     val io = IO(new Bundle{
         val pc = Input(UInt(config.XLEN.W))
         val update = Input(Bool())
-        val wbu_npc = Input(UInt(config.XLEN.W))
+        val exu_npc = Input(UInt(config.XLEN.W))
         val npc = Output(UInt(config.XLEN.W))
 
     })
     // 推测为PC+4
-    io.npc := Mux(io.update, io.wbu_npc, io.pc + 4.U)
+    io.npc := Mux(io.update, io.exu_npc, io.pc + 4.U)
 }
