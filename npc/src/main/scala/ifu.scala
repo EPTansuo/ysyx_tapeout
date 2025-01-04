50ba573c8c933cc709b4aaeda82bbb15f0fd87a7
tracer-ysyx <tracer@ysyx.org>
2025-01-04 16:01:25 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  16:01:25 up  5:58,  1 user,  load average: 1.75, 1.66, 1.62

--- npc/src/main/scala/ifu.scala
+++ npc/src/main/scala/ifu.scala
@@ -152,7 +152,7 @@ class IFU(config: NPCConfig) extends Module {
     when(io.flush || flush){
       flush_cnt := flush_cnt + 1.U
     }
-    when(io.out.valid && io.out.ready && ~flush){
+    when(io.out.valid && io.out.ready && inst(6,0) === "b1100011".U){
       branch_cnt := branch_cnt + 1.U
     }
     when(io.flush ^ flush){
