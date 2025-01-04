27ef454b0a67a3c6070ce4b1c8500fe6718371ff
tracer-ysyx <tracer@ysyx.org>
2025-01-04 14:57:06 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  14:57:06 up  4:53,  1 user,  load average: 2.37, 2.31, 1.98

--- npc/src/main/scala/ifu.scala
+++ npc/src/main/scala/ifu.scala
@@ -78,8 +78,8 @@ class IFU(config: NPCConfig) extends Module {
   immBExtend := immB.asUInt
   when(io.out.valid && io.out.ready && ~flush){
     when(inst(6,0) === "b1100011".U){
-      pc := pc + Mux(immBExtend(config.XLEN-1), immBExtend, 4.U);
-      printf("sign: %d, immB: %d, pc: %d\n", immBExtend(config.XLEN-1), immBExtend, pc)
+      pc := pc + Mux(inst(31), immBExtend, 4.U);
+      printf("sign: %d, immB: %d, pc: %d\n", inst(31), immBExtend, pc)
     }.otherwise{
       pc :=  pc + 4.U//bpu.io.npc
     }
