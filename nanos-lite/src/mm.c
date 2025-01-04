a205185bce451b8f9fe150e94057c32f49c48606
tracer-ysyx <tracer@ysyx.org>
2025-01-04 23:00:41 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  23:00:41 up 12:57,  1 user,  load average: 4.11, 3.80, 3.61

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -9,11 +9,11 @@ void* new_page(size_t nr_page) {
   return ret;
 }
 
-#ifdef HAS_VME
+//#ifdef HAS_VME
 static void* pg_alloc(int n) {
   return NULL;
 }
-#endif
+//#endif
 
 void free_page(void *p) {
   panic("not implement yet");
@@ -24,13 +24,25 @@ void free_page(void *p) {
 int mm_brk(uintptr_t brk) {
 
 
-  if(brk < ROUNDUP(heap.start, PGSIZE)){
-    return -1;
+  // if(brk < ROUNDUP(heap.start, PGSIZE)){
+  //   return -1;
+  // }
+  // current->max_brk = brk;
+  // Log("brk set to %p", (void*)brk);
+
+  if (current->max_brk == 0) {
+    current->max_brk = brk;
+    return 0;
+  }
+  if (brk < current->max_brk) return -1;
+  uint32_t oldVpn = current->max_brk / PGSIZE;
+  uint32_t newVpn = brk / PGSIZE;
+  for (uint32_t i = oldVpn + 1; i <= newVpn; i++) {
+    void *vaddr = (void *)(i * PGSIZE);
+    void *paddr = pg_alloc(PGSIZE);
+    map(&current->as, vaddr, paddr, 1);
   }
   current->max_brk = brk;
-  Log("brk set to %p", (void*)brk);
-
-
   return 0;
 
 }
