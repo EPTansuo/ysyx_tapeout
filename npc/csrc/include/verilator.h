7b7aea779ae15ff0ccca285a1eb7e1c52c41119b
EPTansuo <eptansuo@163.com>
2025-01-04 12:36:31 +0800

    finish simple pipeline for soc fixed bugs for icache

--- npc/csrc/include/verilator.h
+++ npc/csrc/include/verilator.h
@@ -82,7 +82,8 @@ extern VTOP_NAME* top ;
                         (top->ysyx_23060246->cpu_npc))
 
 #define REGS (NPC_CPU->regfile->regs_ext->Memory)
-#define PC (NPC_CPU->wbu->io_in_bits_npc)
+#define PC (NPC_CPU->wbu->io_in_bits_pc)
+#define NPC (NPC_CPU->wbu->io_in_bits_npc)
 #define CSR (NPC_CPU->exu->csr)
 #define WBU_VALID (NPC_CPU->wbu->wbu_valid)
 // #define INST (NPC_CPU->ifu->io_out_bits_inst)
