1e2ebcbed6b5d7971e35643089d692d9697e7147
tracer-ysyx <tracer@ysyx.org>
2025-01-04 20:11:27 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  20:11:27 up 10:08,  1 user,  load average: 5.67, 4.93, 4.14

--- nemu/src/utils/btrace.c
+++ nemu/src/utils/btrace.c
@@ -1,7 +1,13 @@
 #include <common.h>
 #include <fmt-def.h>
 
+void btrace_init(){
+    printf("BTRACE: %s\n", ANSI_FMT("ON", ANSI_FG_GREEN));
+}
 
 void btrace(word_t pc, uint32_t inst, bool taken){
+  static bool isfirst = true;
+  if(isfirst){ isfirst = false; btrace_init(); return;}
+  
   printf("BTRACE: pc: " FMT_WORD_HEX_WIDTH " inst: 0x%08x taken: %d\n", pc, inst, taken);
 }
