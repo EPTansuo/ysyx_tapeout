d498fb9ea504dc1c02a47f15694729fe0c305ccc
tracer-ysyx <tracer@ysyx.org>
2025-01-04 23:44:48 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  23:44:48 up 13:41,  1 user,  load average: 5.33, 4.83, 4.56

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -82,9 +82,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
-  printf("start write\n");
   if(file_table[fd].write != 0) {
-    Log("write to device");
     size_t dev_ret = file_table[fd].write(buf, file_table[fd].disk_offset, len);
     return dev_ret;
   }
@@ -93,7 +91,6 @@ size_t fs_write(int fd, const void *buf, size_t len) {
   if(offset + len > file_table[fd].size) {
     panic("Write failed: Write beyond the end of file!");
   }
-  Log("write to ramdisk");
   size_t ret =  ramdisk_write(buf, file_table[fd].disk_offset + offset, len);
   file_table[fd].open_offset += ret;
   return ret;
