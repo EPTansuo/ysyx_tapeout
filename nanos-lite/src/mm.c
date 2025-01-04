b7e6b5d800b7fdc049568c88917dc3c2e02998fb
tracer-ysyx <tracer@ysyx.org>
2025-01-04 23:24:40 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  23:24:40 up 13:21,  1 user,  load average: 5.52, 5.22, 4.53

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -20,18 +20,21 @@ void free_page(void *p) {
 }
 
 /* The brk() system call handler. */
-
 int mm_brk(uintptr_t brk) {
-
-
-  if(brk < ROUNDUP(heap.start, PGSIZE)){
-    return -1;
+  if (current->max_brk == 0) {
+    current->max_brk = brk;
+    return 0;
   }
-  current->max_brk = brk;
-  //Log("brk set to %p", (void*)brk);
-
+  if (brk > current->max_brk) {
+    intptr_t prevbrk = current->max_brk + PGSIZE ;
+    while (prevbrk < brk) {
+      void *p = new_page(1);
+       map(&current->as, (void *)(prevbrk), p, 0b111);
+      prevbrk += PGSIZE;
+    }
+    current->max_brk = brk;
+  }
   return 0;
-
 }
 
 void init_mm() {
