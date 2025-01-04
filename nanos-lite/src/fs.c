b23be3cf5375cc1594aa58bd2bfa1ad1a2034001
tracer-ysyx <tracer@ysyx.org>
2025-01-04 21:44:21 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  21:44:21 up 11:40,  1 user,  load average: 5.59, 5.39, 4.72

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -68,10 +68,10 @@ size_t fs_read(int fd, void *buf, size_t len) {
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
-  if(file_table[fd].write == 0) {
-    return file_table[fd].write(buf, file_table[fd].disk_offset, len);
+  if(file_table[fd].write != 0) {
+    size_t dev_ret = file_table[fd].write(buf, file_table[fd].disk_offset, len);
+    return dev_ret;
   }
-
 
   size_t offset = file_table[fd].open_offset;
   if(offset + len > file_table[fd].size) {
