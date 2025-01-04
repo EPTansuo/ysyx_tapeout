af5c3d7cff0a4abd266759620d5319ada9266aae
tracer-ysyx <tracer@ysyx.org>
2025-01-04 17:35:33 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  17:35:33 up  7:32,  1 user,  load average: 3.11, 2.49, 2.47

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -172,11 +172,11 @@ static int decode_exec(Decode *s) {
   if (icache[index].pc == s->pc ) {
         if(icache[index].label != NULL){
           s->dnpc = s->pc+4;
-#ifdef CONFIG_TRACE
+//#ifdef CONFIG_TRACE
           s->snpc = s->pc+4;
           s->isa.inst.val = icache[index].inst;
           icache_hit++;
-#endif 
+//#endif 
           goto *icache[index].label;
         }
   }
