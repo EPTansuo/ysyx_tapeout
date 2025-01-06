ab3d09d0f96b6e95e77ad8d7955c90c456e59dce
tracer-ysyx <tracer@ysyx.org>
2025-01-06 20:45:13 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  20:45:13 up  6:53,  1 user,  load average: 2.27, 2.45, 2.62

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -80,17 +80,33 @@ void NDL_OpenCanvas(int *w, int *h) {
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
-  int fb = open("/dev/fb", 0, 0);
-  int width, height;
+  // int fb = open("/dev/fb", 0, 0);
+  // int width, height;
+  // get_screen_size(&width, &height);
+
+  // int offset = x + y * width;
+  // lseek(fb, offset * 4, SEEK_SET);
+
+  // for (int i = 0; i < h; i++) {
+  //   write(fb, pixels + i * w, w * 4);
+  //   offset += width;
+  //   lseek(fb, offset * 4, SEEK_SET);
+  // }
+
+    int fd = open("/dev/fb", 0 ,0);
+  uint32_t *p = pixels;
+  int offset,width,height,i;
   get_screen_size(&width, &height);
-
-  int offset = x + y * width;
-  lseek(fb, offset * 4, SEEK_SET);
-
-  for (int i = 0; i < h; i++) {
-    write(fb, pixels + i * w, w * 4);
-    offset += width;
-    lseek(fb, offset * 4, SEEK_SET);
+  int basex = (width - screen_w)/2;
+  int basey = (height - screen_h)/2;
+  x = basex + x;
+  y = basey + y;
+  offset = x + y * width;
+  lseek(fd, offset, SEEK_SET);
+  for(i = 0;i < h;i ++){
+    write(fd, p, w);
+    lseek(fd, width - w, SEEK_CUR);
+    p += w;
   }
 }
 
