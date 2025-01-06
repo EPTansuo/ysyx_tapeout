243f24816fc566e8cf67fc4923bbaa5c44bec708
tracer-ysyx <tracer@ysyx.org>
2025-01-06 20:27:29 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  20:27:29 up  6:36,  1 user,  load average: 2.39, 2.74, 2.77

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -29,7 +29,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T config = io_read(AM_GPU_CONFIG);
   char buffer[64];
   int str_len = sprintf(buffer,"WIDTH : %d\nHEIGHT: %d\n",config.width,config.height);
-  Log("dispinfo_read: %s",buf);
+  // Log("dispinfo_read: %s",buf);
   int i = 0;
   for(; i < len && i <= str_len; i++) {
     ((char *)buf)[i] = buffer[i];
