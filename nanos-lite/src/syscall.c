1c187e8bf161a5f01b7df33cd65b1fb5b28d0c59
tracer-ysyx <tracer@ysyx.org>
2025-01-06 18:02:48 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  18:02:48 up  4:11,  1 user,  load average: 8.07, 7.58, 6.73

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -35,6 +35,15 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
+#ifdef STRACE
+if(a[0] == SYS_write || a[0] == SYS_read || a[0] == SYS_lseek || a[0] == SYS_close){
+  Log("SYSCALL(%s, %s, 0x%x, 0x%x) = %d", syscalls[a[0]], fs_get_file_name(a[1]), a[2], a[3], c->GPRx);
+  //fs_strace(syscalls[a[0]], a[1], a[2], a[3]);
+}
+else{
+  Log("SYSCALL(%s, 0x%x, 0x%x, 0x%x) = %d", syscalls[a[0]], a[1], a[2], a[3], c->GPRx);
+}
+#endif 
 
 
   switch (a[0]) {
@@ -49,14 +58,5 @@ void do_syscall(Context *c) {
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
-  #ifdef STRACE
-if(a[0] == SYS_write || a[0] == SYS_read || a[0] == SYS_lseek || a[0] == SYS_close){
-  Log("SYSCALL(%s, %s, 0x%x, 0x%x) = %d", syscalls[a[0]], fs_get_file_name(a[1]), a[2], a[3], c->GPRx);
-  //fs_strace(syscalls[a[0]], a[1], a[2], a[3]);
-}
-else{
-  Log("SYSCALL(%s, 0x%x, 0x%x, 0x%x) = %d", syscalls[a[0]], a[1], a[2], a[3], c->GPRx);
-}
-#endif 
 
 }
