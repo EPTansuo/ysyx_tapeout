fee3eb5cde7493112cf1aedb00170cd3aedf8a41
tracer-ysyx <tracer@ysyx.org>
2025-01-06 21:02:01 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  21:02:01 up  7:10,  1 user,  load average: 3.09, 3.27, 3.00

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -108,7 +108,10 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   uint32_t *p = pixels;
   int offset,width,height,i;
   get_screen_size(&width, &height);
-
+int basex = (width - screen_w)/2;
+  int basey = (height - screen_h)/2;
+  x = basex + x;
+  y = basey + y;
   offset = x + y * width;
 
   lseek(fd, offset, SEEK_SET);
