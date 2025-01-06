766f60581c1d36967b9cbfa607059a93cef4d52f
tracer-ysyx <tracer@ysyx.org>
2025-01-06 19:18:43 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  19:18:43 up  5:27,  1 user,  load average: 2.20, 2.39, 2.41

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <NDL.h>
+
 
 int main() {
-    struct timeval tv, last_tv;
-    long seconds, useconds;
-    int interval = 500000;  // 0.5 秒，以微秒为单位
-
-    gettimeofday(&last_tv, NULL);
+    size_t ticks, last_ticks;
+	size_t interval = 500000; // 0.5s
+	NDL_Init(0);
+	last_ticks  = NDL_GetTicks();
 
     while (1) {
-        gettimeofday(&tv, NULL);
-        seconds  = tv.tv_sec  - last_tv.tv_sec;
-        useconds = tv.tv_usec - last_tv.tv_usec;
-        if (useconds < 0) {
-            useconds += 1000000;
-            seconds--;
-        }
-        if ((seconds * 1000000 + useconds) >= interval) {
+		ticks = NDL_GetTicks();
+        if ((ticks - last_ticks) >= interval) {
             printf("hello\n");
-            last_tv = tv;
+            last_ticks = ticks;
         }
 
     }
