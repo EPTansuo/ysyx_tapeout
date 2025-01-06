536aa5fbb42f060ce59c81d6bb2992f88ffb5bc4
tracer-ysyx <tracer@ysyx.org>
2025-01-06 18:45:43 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  18:45:43 up  4:54,  1 user,  load average: 2.58, 1.87, 2.83

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -36,6 +36,7 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
+printf("%s\n", syscalls[a[0]]);
 
 
   switch (a[0]) {
@@ -51,7 +52,7 @@ void do_syscall(Context *c) {
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
-  #ifdef STRACE
+#ifdef STRACE
 if(a[0] == SYS_write || a[0] == SYS_read || a[0] == SYS_lseek || a[0] == SYS_close){
   Log("SYSCALL(%s, %s, 0x%x, 0x%x) = %d", syscalls[a[0]], fs_get_file_name(a[1]), a[2], a[3], c->GPRx);
   //fs_strace(syscalls[a[0]], a[1], a[2], a[3]);
