e6b400c9856110f29d2dbfc1b132586ae2183a52
tracer-ysyx <tracer@ysyx.org>
2025-01-06 21:05:46 +0800

    > compile NEMU ysyx_23060246 韩炳晋 Linux archlinux 6.6.68-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 15:16:06 +0000 x86_64 GNU/Linux  21:05:46 up  7:14,  1 user,  load average: 2.31, 2.68, 2.82

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -73,7 +73,8 @@ int fs_open(const char *pathname, int flags, int mode) {
 
 size_t fs_read(int fd, void *buf, size_t len) {
   if(file_table[fd].read !=0){
-    size_t dev_ret = file_table[fd].read(buf, file_table[fd].disk_offset, len);
+    size_t dev_ret = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    file_table[fd].open_offset += dev_ret;
     return dev_ret;
   }
 
@@ -92,7 +93,8 @@ size_t fs_read(int fd, void *buf, size_t len) {
 
 size_t fs_write(int fd, const void *buf, size_t len) {
   if(file_table[fd].write != 0) {
-    size_t dev_ret = file_table[fd].write(buf, file_table[fd].disk_offset, len);
+    size_t dev_ret = file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    file_table[fd].open_offset += dev_ret;
     return dev_ret;
   }
 
