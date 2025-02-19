e63ae5b116a86e383173e2422d84b993692b7196
tracer-ysyx <tracer@ysyx.org>
2025-02-19 10:01:22 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.13-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 08 Feb 2025 15:16:04 +0000 x86_64 GNU/Linux  10:01:22 up 17:57,  1 user,  load average: 3.68, 2.07, 1.33

--- npc/csrc/include/verilator.h
+++ npc/csrc/include/verilator.h
@@ -40,7 +40,9 @@ _(_Dpi) IFDEF(CONFIG_USE_SOC, _(ysyxSoCFull)) IFNDEF(CONFIG_USE_SOC, _(ysyxSoCAS
 #include "VysyxSoCFull_IDU.h"
 #include "VysyxSoCFull_IFU.h"
 #include "VysyxSoCFull_CPU.h"
+#ifdef CONFIG_USE_ICACHE
 #include "VysyxSoCFull_ICache.h"
+#endif 
 #else
 #include "Vysyx_23060246.h"
 #include "Vysyx_23060246__Dpi.h"
@@ -58,7 +60,9 @@ _(_Dpi) IFDEF(CONFIG_USE_SOC, _(ysyxSoCFull)) IFNDEF(CONFIG_USE_SOC, _(ysyxSoCAS
 #include "Vysyx_23060246_LSU.h"
 #include "Vysyx_23060246_IDU.h"
 #include "Vysyx_23060246_IFU.h"
+#ifdef CONFIG_USE_ICACHE
 #include "Vysyx_23060246_ICache.h"
+#endif 
 //#include "Vysyx_23060246_CPU.h"
 #endif 
 
