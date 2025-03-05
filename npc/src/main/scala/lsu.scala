a387314da0d14fb06cbb90910d765a3da9279259
tracer-ysyx <tracer@ysyx.org>
2025-03-05 17:51:48 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  17:51:48 up 1 day,  8:30,  1 user,  load average: 4.59, 3.96, 3.30

--- npc/src/main/scala/lsu.scala
+++ npc/src/main/scala/lsu.scala
@@ -99,11 +99,11 @@ val s_idle :: s_exe :: s_read :: s_wait_read :: s_read_2 :: s_wait_read_2 :: s_w
     ))
 
 
-    var ld_sel_reg = RegInit(0.U(3.W))
-    when(state === s_wait_ready){
-        ld_sel_reg := ctrlsig.ld_sel
-    }
-    io.ld_sel := ld_sel_reg
+    // var ld_sel_reg = RegInit(0.U(3.W))
+    // when(state === s_wait_ready){
+    //     ld_sel_reg := ctrlsig.ld_sel
+    // }
+    io.ld_sel := ctrlsig.ld_sel//ld_sel_reg
 
     io.out.valid := state === s_wait_ready
     io.in.ready := state === s_idle
