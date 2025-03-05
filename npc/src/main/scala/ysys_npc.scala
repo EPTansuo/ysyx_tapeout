0e563fb5edea70d9e506bba2deff53771abf69d2
tracer-ysyx <tracer@ysyx.org>
2025-03-05 17:36:53 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  17:36:53 up 1 day,  8:15,  1 user,  load average: 3.81, 2.94, 2.23

--- npc/src/main/scala/ysys_npc.scala
+++ npc/src/main/scala/ysys_npc.scala
@@ -152,9 +152,10 @@ class ysyx_npc(config: NPCConfig) extends Module {
     dontTouch(isRAW_loaduse)
 
     idu.io.stall := stall
-    stall := RAW_with_Fwd || RegNext(RAW_with_Fwd) || isRAW_loaduse
+    val RAW_with_Fwd_delay = RegNext(RAW_with_Fwd)
+    stall := RAW_with_Fwd || RAW_with_Fwd_delay || isRAW_loaduse
     dontTouch(RAW_with_Fwd)
-    
+
     if(config.PERF_CNT){
         val loaduse_cnt = RegInit(0.U(64.W))
         when(isRAW_loaduse){
