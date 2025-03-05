626b29ca5fbde0b91b4be9e3c1739075a6915045
tracer-ysyx <tracer@ysyx.org>
2025-03-05 14:03:16 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  14:03:16 up 1 day,  4:41,  1 user,  load average: 7.36, 5.68, 4.50

--- npc/src/main/scala/regfile.scala
+++ npc/src/main/scala/regfile.scala
@@ -28,10 +28,10 @@ class Regfile(config: NPCConfig) extends Module{
     var regs = Mem(config.REG_NUM, UInt(config.XLEN.W))
 
     // read data with internal forwarding 
-    io.read1.data := Mux(io.write.addr === io.read1.addr && io.write.addr =/= 0.U, 
-                            io.write.data, regs(io.read1.addr))
-    io.read2.data := Mux(io.write.addr === io.read2.addr && io.write.addr =/= 0.U, 
-                            io.write.data, regs(io.read2.addr))
+    // io.read1.data := Mux(io.write.addr === io.read1.addr && io.write.addr =/= 0.U, 
+    //                         io.write.data, regs(io.read1.addr))
+    // io.read2.data := Mux(io.write.addr === io.read2.addr && io.write.addr =/= 0.U, 
+    //                         io.write.data, regs(io.read2.addr))
     
     when(io.write.en) {
         regs(io.write.addr) := Mux(io.write.addr === 0.U, 0.U, io.write.data)
