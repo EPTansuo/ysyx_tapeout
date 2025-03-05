0b03172c8e901ef41ab4a96a3c40360b0dc520ab
tracer-ysyx <tracer@ysyx.org>
2025-03-05 16:04:32 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  16:04:32 up 1 day,  6:43,  1 user,  load average: 2.35, 1.43, 1.94

--- npc/src/main/scala/forward.scala
+++ npc/src/main/scala/forward.scala
@@ -41,7 +41,9 @@ class Forward(config: NPCConfig) extends Module {
     val exu_forward_a = forwardFrom(io.idu_rs1, io.exu_rd, io.exu_itype)
     val lsu_forward_a = forwardFrom(io.idu_rs1, io.lsu_rd, io.lsu_itype)
     val wbu_forward_a = forwardFrom(io.idu_rs1, io.lsu_rd, io.wbu_itype)
-
+    dontTouch(exu_forward_a)
+    dontTouch(lsu_forward_a)
+    dontTouch(wbu_forward_a)
     io.forward_A := Mux(exu_forward_a, forward_sel.FWD_EXU, 
                     Mux(lsu_forward_a, forward_sel.FWD_LSU, 
                     Mux(wbu_forward_a, forward_sel.FWD_WBU, forward_sel.FWD_XX)))
@@ -49,7 +51,9 @@ class Forward(config: NPCConfig) extends Module {
     val exu_forward_b = forwardFrom(io.idu_rs2, io.exu_rd, io.exu_itype)
     val lsu_forward_b = forwardFrom(io.idu_rs2, io.lsu_rd, io.lsu_itype)
     val wbu_forward_b = forwardFrom(io.idu_rs2, io.lsu_rd, io.wbu_itype)
-
+    dontTouch(exu_forward_b)
+    dontTouch(lsu_forward_a)
+    dontTouch(wbu_forward_b)
     io.forward_B := Mux(exu_forward_b, forward_sel.FWD_EXU, 
                     Mux(lsu_forward_b, forward_sel.FWD_LSU, 
                     Mux(wbu_forward_b, forward_sel.FWD_WBU, forward_sel.FWD_XX)))
