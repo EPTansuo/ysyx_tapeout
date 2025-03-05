a77123c389608f31bd4cd42da9ad97e3d37aeb54
tracer-ysyx <tracer@ysyx.org>
2025-03-05 14:35:27 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  14:35:27 up 1 day,  5:14,  1 user,  load average: 7.99, 7.29, 6.72

--- npc/src/main/scala/exu.scala
+++ npc/src/main/scala/exu.scala
@@ -51,9 +51,8 @@ class EXU(config: NPCConfig) extends Module{
 
     val state = RegInit(s_idle)         
     state := MuxLookup(state, s_idle)(Seq(
-        s_idle -> Mux(io.in.valid, s_wait_ready, s_idle),
-        // s_idle -> Mux(io.in.valid, s_exe, s_idle),
-        // s_exe  -> s_wait_ready, // Reserve more time for EXU
+        s_idle -> Mux(io.in.valid, s_exe, s_idle),
+        s_exe  -> s_wait_ready, // Reserve more time for EXU
         s_wait_ready -> Mux(io.out.ready, s_idle, s_wait_ready)
     ))
 
@@ -87,33 +86,21 @@ class EXU(config: NPCConfig) extends Module{
 
     //val imm_out_reg = RegInit(0.U(32.W))
     //imm_out_reg := immGen.io.out 
-    // val alu_A_reg = RegInit(0.U(xlen.W))
-    // alu_A_reg := MuxLookup(ctrlsig.A_sel, 0.U(xlen.W))(Seq(
-    //     A_RS1 -> src1,
-    //     A_PC  -> pc
-    //     )
-    // )
-    // alu.io.A := alu_A_reg
-
-    alu.io.A := MuxLookup(ctrlsig.A_sel, 0.U(xlen.W))(Seq(
+    val alu_A_reg = RegInit(0.U(xlen.W))
+    alu_A_reg := MuxLookup(ctrlsig.A_sel, 0.U(xlen.W))(Seq(
         A_RS1 -> src1,
         A_PC  -> pc
         )
     )
+    alu.io.A := alu_A_reg
 
-    // val alu_B_reg = RegInit(0.U(xlen.W))
-    // alu_B_reg := MuxLookup(ctrlsig.B_sel, 0.U(xlen.W))(Seq(
-    //     B_RS2 -> src2,
-    //     B_IMM -> immGen.io.out 
-    //     )
-    // )
-    // alu.io.B := alu_B_reg
-
-    alu.io.B := MuxLookup(ctrlsig.B_sel, 0.U(xlen.W))(Seq(
+    val alu_B_reg = RegInit(0.U(xlen.W))
+    alu_B_reg := MuxLookup(ctrlsig.B_sel, 0.U(xlen.W))(Seq(
         B_RS2 -> src2,
         B_IMM -> immGen.io.out 
         )
     )
+    alu.io.B := alu_B_reg
 
     alu.io.aluop := ctrlsig.alu_op
 
@@ -124,17 +111,14 @@ class EXU(config: NPCConfig) extends Module{
     csr.io.inst := inst
     csr.io.pc := pc
     csr.io.cmd := sig_csr_cmd
-    // csr.io.in := src1_reg  //目前还未用到立即数  WARNING
-    csr.io.in := src1  //目前还未用到立即数  WARNING
+    csr.io.in := src1_reg  //目前还未用到立即数  WARNING
     csr.io.update_enable := RegNext(io.out.valid)
     dontTouch(csr.io)  //任何时候都不优化
 
     val branch = Module(new Branch(xlen))
     branch.io.br_sel := ctrlsig.br_sel
-    // branch.io.src1 := src1_reg
-    // branch.io.src2 := src2_reg
-    branch.io.src1 := src1
-    branch.io.src2 := src2
+    branch.io.src1 := src1_reg
+    branch.io.src2 := src2_reg
 
     val npc = MuxCase(
         pc + 4.U,  
@@ -155,13 +139,10 @@ class EXU(config: NPCConfig) extends Module{
     // io.forward.valid := state === s_exe && ctrlsig.ld_sel =/= LD_XX && ctrlsig.wb_sel =/= WB_XX
 
 
-
     io.out.bits.csr_out  := csr.io.out
     io.out.bits.rd_addr := rd_addr
-    // io.out.bits.src1 := src1_reg
-    // io.out.bits.src2 := src2_reg 
-    io.out.bits.src1 := src1
-    io.out.bits.src2 := src2
+    io.out.bits.src1 := src1_reg
+    io.out.bits.src2 := src2_reg 
     io.out.bits.alu_out := alu.io.out
     io.out.bits.pc := pc
     io.out.bits.inst := inst
