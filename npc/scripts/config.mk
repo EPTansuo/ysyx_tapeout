fa5b52812d603ce15234d4f9a77874026a977c65
tracer-ysyx <tracer@ysyx.org>
2025-03-05 13:03:22 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  13:03:22 up 1 day,  3:41,  1 user,  load average: 2.75, 2.53, 2.44

--- npc/scripts/config.mk
+++ npc/scripts/config.mk
@@ -23,7 +23,7 @@ ifeq ($(CONFIG_DIFFTEST),y)
 #CFLAGS += -I$(CONFIG_LIBRARY_PATH)
 ifeq ($(CONFIG_DIFFTEST_REF_NEMU),y)
 ifeq ($(CONFIG_USE_SOC),y)
-	LDFLAGS += -L$(NEMU_HOME)/build/riscv32e-nemu-ysyxsoc-so
+	LDFLAGS += -L$(NPC_HOME)/difftest/riscv32e-nemu-ysyxsoc-so
 else
 	LDFLAGS += -L$(NPC_HOME)/difftest/riscv32e-nemu-npc-so
 endif
