dd7c32c4fb96e664715a9b3e410cc3bd6e951de6
tracer-ysyx <tracer@ysyx.org>
2025-03-06 15:26:03 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  15:26:03 up 2 days,  6:04,  1 user,  load average: 8.22, 8.21, 5.97

--- npc/csrc/cpu/per_cnt.cpp
+++ npc/csrc/cpu/per_cnt.cpp
@@ -139,10 +139,10 @@ void perf_statistic(){
 
     printf("\n--------------------------------------- BPU --------------------------------------\n");
     PRINT_PERF("Branch CNT", branch_cnt, (double)branch_cnt/inst_cnt*100, "% of inst");
-    PRINT_PERF("  Jump CNT", branch_cnt, (double)inst_jump_cnt/inst_cnt*100, "% of inst");
+    PRINT_PERF("  Jump CNT", inst_jump_cnt, (double)inst_jump_cnt/inst_cnt*100, "% of inst");
     PRINT_PERF("Branch Predict Succ CNT", branch_cnt-branch_predict_failed_cnt,
-                 (double)(branch_cnt-branch_predict_failed_cnt)/(inst_jump_cnt + inst_branch_cnt)*100, "% of branch");
-                 
+                 (double)(branch_predict_failed_cnt - inst_jump_cnt)/( branch_cnt)*100, "% of branch");
+
     fclose(fp);
 #endif // CONFIG_PERF_CNT
 }
