8d16fb0f00da0cc71a2c6d5b6bec77e95976a827
tracer-ysyx <tracer@ysyx.org>
2025-03-06 15:00:04 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  15:00:04 up 2 days,  5:38,  1 user,  load average: 6.76, 5.32, 4.06

--- npc/src/main/scala/hazard.scala
+++ npc/src/main/scala/hazard.scala
@@ -15,7 +15,8 @@ class ControlHazard(config: NPCConfig) extends Module{
 
     val ifu_flush = io.ifu_pc.valid && (io.exu_npc.bits =/= io.ifu_pc.bits)
     val idu_flush = io.idu_pc.valid && (io.exu_npc.bits =/= io.idu_pc.bits)
-
+    dontTouch(idu_flush)
+    dontTouch(ifu_flush)
     io.flush := (ifu_flush || idu_flush) && io.exu_npc.valid
 
     io.ifu_pc.ready := true.B
