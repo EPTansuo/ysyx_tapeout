7657bfd6904eedf377a559410941fcf3e223ef8c
tracer-ysyx <tracer@ysyx.org>
2025-03-07 11:35:48 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.17-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 27 Feb 2025 14:12:30 +0000 x86_64 GNU/Linux  11:35:48 up 15:26,  1 user,  load average: 4.11, 3.25, 2.66

--- npc/src/main/scala/bpu.scala
+++ npc/src/main/scala/bpu.scala
@@ -67,15 +67,16 @@ class BPU(config:NPCConfig) extends Module{
 
     val branch_taken = io.exu_npc.bits === io.idu_pc 
     dontTouch(branch_taken)
-    val isBranch = io.exu_pc =/= io.exu_npc.bits
+    val isBranch = io.exu_pc =/= io.exu_npc.bits + 4.U
     dontTouch(isBranch)
     when(io.exu_npc.valid && isBranch){
         when(branch_taken) {
-            btb.counter(tag) := Mux(btb.counter(tag) === 3.U, 3.U, 
-                                            btb.counter(tag) + 1.U)
-        }.otherwise {
             btb.counter(tag) := Mux(btb.counter(tag) === 0.U, 0.U, 
-                                            btb.counter(tag) - 1.U)
+                                btb.counter(tag) - 1.U)
+
+        }.otherwise {
+            btb.counter(tag) := Mux(btb.counter(tag) === 3.U, 3.U, 
+                                btb.counter(tag) + 1.U)
         }
     }
 
