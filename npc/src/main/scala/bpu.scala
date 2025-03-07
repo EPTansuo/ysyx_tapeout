d606acb082dc883087dc02c6efef894194b61960
tracer-ysyx <tracer@ysyx.org>
2025-03-07 10:32:48 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.17-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 27 Feb 2025 14:12:30 +0000 x86_64 GNU/Linux  10:32:48 up 14:23,  1 user,  load average: 4.48, 3.71, 2.77

--- npc/src/main/scala/bpu.scala
+++ npc/src/main/scala/bpu.scala
@@ -18,21 +18,31 @@ class BPU(config:NPCConfig) extends Module{
     val pcWidth = params.pcWidth
     val npcWidth = params.npcWidth
 
+    val cntWidth = 2
+
     val btb = new Bundle {
-        val pc = RegInit(VecInit(Seq.fill(entrySize)(0.U(pcWidth.W)))) // use the lowest 8-bit
+        val pc = RegInit(VecInit(Seq.fill(entrySize)(0.U(pcWidth.W))))
         val npc = RegInit(VecInit(Seq.fill(entrySize)(0.U(npcWidth.W))))
+        val counter = RegInit(VecInit(Seq.fill(entrySize)(0.U(cntWidth.W)))) // 2-bit saturating counter
     }
+    
 
     val tag = RegInit(0.U(tagWidth.W))
 
 
     val npc = Wire(UInt(config.XLEN.W))
 
-    npc := io.pc + 4.U
+    val branch_taken = io.exu_npc =/= io.exu_pc + 4.U
+
+    // npc := io.pc + 4.U
     for (i <- 0 until entrySize) {
         when(btb.pc(i) === io.pc(pcWidth-1,0)) {
-            npc := Cat(io.pc(config.XLEN-1, npcWidth), btb.npc(i))
-            tag := (i).U
+            when(btb.counter(i)(cntWidth-1)){
+                npc := Cat(io.pc(config.XLEN-1, npcWidth), btb.npc(i))
+            }.otherwise{
+                npc := io.pc + 4.U
+            }
+        tag := (i).U 
         }
     }
 
@@ -42,6 +52,14 @@ class BPU(config:NPCConfig) extends Module{
         btb.pc(replace_addr) := io.exu_pc
         btb.npc(replace_addr) := io.exu_npc
         tag := replace_addr
+
+        when(branch_taken) {
+            btb.counter(replace_addr) := Mux(btb.counter(replace_addr) === 3.U, 3.U, 
+                                            btb.counter(replace_addr) + 1.U)
+        }.otherwise {
+            btb.counter(replace_addr) := Mux(btb.counter(replace_addr) === 0.U, 0.U, 
+                                            btb.counter(replace_addr) - 1.U)
+        }
     }
 
     io.npc := npc
