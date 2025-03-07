646a98b2ef20b4e3a2b4abb5a7843195b9c84c5c
tracer-ysyx <tracer@ysyx.org>
2025-03-07 19:26:19 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.17-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 27 Feb 2025 14:12:30 +0000 x86_64 GNU/Linux  19:26:19 up 23:16,  1 user,  load average: 1.75, 1.39, 1.80

--- npc/csrc/include/autoconf.h
+++ npc/csrc/include/autoconf.h
@@ -6,10 +6,11 @@
 #define CONFIG_PERF_CNT 1
 #define CONFIG_CC_O2 1
 #define CONFIG_CC_OPT "-O2"
+#define CONFIG_WAVE_DUMP 1
+#define CONFIG_WAVE_VCD 1
 #define CONFIG_WAVE_FILE_TYPE "none"
 #define CONFIG_USE_SOC 1
 #define CONFIG_SOC_EN "enable"
-#define CONFIG_USE_NVBOARD 1
 #define CONFIG_RESET_VECTOR 0x30000000
 #define CONFIG_DIFFTEST_REF_PATH "none"
 #define CONFIG_DIFFTEST_REF_NAME "none"
