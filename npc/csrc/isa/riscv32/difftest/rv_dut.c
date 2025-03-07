0072a0908f16d8707b22c7e745643b158b84d246
tracer-ysyx <tracer@ysyx.org>
2025-03-07 15:03:51 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.17-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 27 Feb 2025 14:12:30 +0000 x86_64 GNU/Linux  15:03:51 up 18:54,  1 user,  load average: 1.84, 1.42, 1.41

--- npc/csrc/isa/riscv32/difftest/rv_dut.c
+++ npc/csrc/isa/riscv32/difftest/rv_dut.c
@@ -33,7 +33,7 @@ extern const char *regs[];
 
 // 为了匹配，所以让ref_r使用cpu_state_buf推迟了一个周期
 // 改了，现在不推迟了
-// 现在又要推迟了!! 不过只是csr推迟
+// 现在又要推迟了!! 不过是让npc_cpu推迟一个周期
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int i =0;
   bool succ = true;
