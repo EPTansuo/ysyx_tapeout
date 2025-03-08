cd82a5b5305d055d94fd9d7ddfee5d55c5d156ce
tracer-ysyx <tracer@ysyx.org>
2025-03-08 11:34:34 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.17-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 27 Feb 2025 14:12:30 +0000 x86_64 GNU/Linux  11:34:34 up 1 day, 15:25,  1 user,  load average: 3.03, 2.17, 1.95

--- npc/csrc/cpu/perf_cnt.cpp
+++ npc/csrc/cpu/perf_cnt.cpp
@@ -12,7 +12,7 @@
 
 
 #define PERF_IFU_CNT(_) \
-_(ifu_cnt) _(flush_cnt) _(branch_predict_failed_cnt)
+_(ifu_cnt) _(flush_cnt) _(branch_predict_failed_cnt) _(flush_cycle_cnt) _(jmp_cnt) _(branch_cnt)
 
 #define PERF_IDU_CNT(_) \
 _(inst_compute_cnt) _(inst_branch_cnt) _(inst_jump_cnt) _(inst_ldst_cnt) _(inst_csr_cnt) \
@@ -134,16 +134,15 @@ void perf_statistic(){
     }
 #endif 
     printf("\n------------------------------------- Pipeline -------------------------------------\n");
-    PRINT_PERF("Flush Cycles CNT", flush_cnt, (double)flush_cnt/cycle_cnt*100, "% of total cycles");
+    PRINT_PERF("Flush Cycles CNT", flush_cycle_cnt, (double)flush_cycle_cnt/cycle_cnt*100, "% of total cycles");
     PRINT_PERF("Stall Cycles CNT", stall_cnt, (double)stall_cnt/cycle_cnt*100, "% of total cycles");
 
     printf("\n--------------------------------------- BPU --------------------------------------\n");
     // PRINT_PERF("Branch CNT", inst_branch_cnt, (double)inst_branch_cnt/inst_cnt*100, "% of inst");
     //PRINT_PERF("  Jump CNT", inst_jump_cnt, (double)inst_jump_cnt/inst_cnt*100, "% of inst");
-    PRINT_PERF("Predict Faild CNT", branch_predict_failed_cnt-inst_jump_cnt, 0.0, "")
-    PRINT_PERF("Predict Succ CNT", inst_branch_cnt - (branch_predict_failed_cnt-inst_jump_cnt),
-                 (double)(inst_branch_cnt - (branch_predict_failed_cnt - inst_jump_cnt))/
-                 ( inst_branch_cnt)*100, "% of branch");
+    PRINT_PERF("Predict Faild CNT", branch_predict_failed_cnt, 0.0, "")
+    PRINT_PERF("Predict Succ CNT", branch_predict_failed_cnt,
+                 (double)(branch_predict_failed_cnt)/( branch_cnt )*100, "% of branch");
 
     fclose(fp);
 #endif // CONFIG_PERF_CNT
