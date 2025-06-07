825675b06aa329fc966c80c51b32b0b736b26749
EPTansuo <eptansuo@163.com>
2025-06-07 10:19:18 +0800

    add prarmeter for scan_breakpoint declaration

--- nemu/include/breakpoint.h
+++ nemu/include/breakpoint.h
@@ -8,6 +8,6 @@ void init_bp_pool();
 bool add_breakpoint(vaddr_t addr);
 bool del_breakpoint(vaddr_t addr);
 
-void scan_breakpoint();
+void scan_breakpoint(vaddr_t addr);
 
 #endif // !__BREAKPOINT_H_
