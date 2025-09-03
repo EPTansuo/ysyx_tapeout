5f3504b99f09329ecc0f4c9d8d70320121a98529
EPTansuo <eptansuo@163.com>
2025-09-03 18:00:59 +0800

    Disable performence counter

--- npc/csrc/include/autoconf.h
+++ npc/csrc/include/autoconf.h
@@ -2,7 +2,6 @@
 #define CONFIG_ISA "riscv32"
 #define CONFIG_RVE 1
 #define CONFIG_USE_BPU 1
-#define CONFIG_PERF_CNT 1
 #define CONFIG_USE_REG0 1
 #define CONFIG_CC_O2 1
 #define CONFIG_CC_OPT "-O2"
