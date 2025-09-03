a1e87cbe2330cfda16f5d2ab97072a9884e3bba4
tracer-ysyx <tracer@ysyx.org>
2025-09-03 17:49:29 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.43-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 20 Aug 2025 17:50:11 +0000 x86_64 GNU/Linux  17:49:29 up 1 day,  7:13,  1 user,  load average: 2.74, 1.78, 1.30

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -16,6 +16,38 @@
 #include <verilator.h>
 
 
+
+#include <signal.h>
+#include <execinfo.h>
+#include <unistd.h>
+#include <cstdio>
+#include <cstdlib>
+
+static void _bt_handler(int sig) {
+  void* frames[128];
+  int n = backtrace(frames, 128);
+  const char* name =
+      sig==SIGABRT? "SIGABRT" :
+      sig==SIGSEGV? "SIGSEGV" :
+      sig==SIGBUS ? "SIGBUS"  :
+      sig==SIGILL ? "SIGILL"  : "SIG";
+  fprintf(stderr, "\n===== %s detected; backtrace (%d frames) =====\n", name, n);
+  backtrace_symbols_fd(frames, n, STDERR_FILENO);
+  _exit(128 + sig); // 避免再次跑析构
+}
+static inline void install_bt_handler() {
+  signal(SIGABRT, _bt_handler);
+  signal(SIGSEGV, _bt_handler);
+  signal(SIGBUS,  _bt_handler);
+  signal(SIGILL,  _bt_handler);
+}
+
+
+
+
+
+
+
 extern  char img[131072];
 
 bool stop = false;
@@ -30,6 +62,7 @@ void stop_sim();
 
 int main(int argc, char **argv)
 {
+  install_bt_handler();
 	Verilated::commandArgs(argc, argv);
 	init_monitor(argc, argv);
 	engine_start();
