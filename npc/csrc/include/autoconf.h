d22d62f364a08a8339f7da87b9ef2db5bf2d435d
tracer-ysyx <tracer@ysyx.org>
2025-09-04 15:23:14 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.43-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 20 Aug 2025 17:50:11 +0000 x86_64 GNU/Linux  15:23:14 up 2 days,  4:47,  1 user,  load average: 2.27, 1.80, 1.99

--- npc/csrc/include/autoconf.h
+++ npc/csrc/include/autoconf.h
@@ -7,6 +7,9 @@
 #define CONFIG_CC_O2 1
 #define CONFIG_CC_OPT "-O2"
 #define CONFIG_WAVE_FILE_TYPE "none"
+#define CONFIG_TRACE 1
+#define CONFIG_TRACE_START 0
+#define CONFIG_TRACE_END 10000
 #define CONFIG_DIFFTEST 1
 #define CONFIG_DIFFTEST_REF_NEMU 1
 #define CONFIG_DIFFTEST_REF_PATH "../nemu/build"
