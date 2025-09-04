8666edab2bf943972c6993822613af413a6d5635
tracer-ysyx <tracer@ysyx.org>
2025-09-04 17:51:44 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.43-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 20 Aug 2025 17:50:11 +0000 x86_64 GNU/Linux  17:51:44 up 2 days,  7:15,  1 user,  load average: 0.80, 0.82, 0.77

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -47,6 +47,7 @@ static void welcome() {
   // IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
   //       "to record the trace. This may lead to a large log file. "
   //       "If it is not necessary, you can disable it in menuconfig"));
+  IFDEF(USE_NVBOARD, Log("USE Nvboard for Simulation."));
   Log("Dump Wave: %s", MUXDEF(CONFIG_WAVE_DUMP, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
   MUXDEF(CONFIG_WAVE_DUMP, Log("Dump File Type: %s", MUXDEF(CONFIG_WAVE_VCD, "VCD", "FST")),);
   Log("Build time: %s, %s", __TIME__, __DATE__);
