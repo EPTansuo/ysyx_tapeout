3bf206107e50cb9788e8291e9440f3ba9729ba23
tracer-ysyx <tracer@ysyx.org>
2025-09-05 21:33:57 +0800

    > sim RTL ysyx_23060246 韩炳晋 Linux archlinux 6.12.43-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 20 Aug 2025 17:50:11 +0000 x86_64 GNU/Linux  21:33:57 up  5:31,  1 user,  load average: 1.23, 0.84, 0.83

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -34,6 +34,8 @@ void putch(char ch) {
 extern char _text_start, _data_start, _data_end,
 _data_src, _text_src, _text_end, _bss_start, _bss_end;
 
+extern char _rodata_start, _rodata_end, _rodata_src;
+
 void __attribute__((section(".bootloader"))) bootloader(){
   char *src = &_text_src;
   char *dst = &_text_start;
@@ -47,10 +49,21 @@ void __attribute__((section(".bootloader"))) bootloader(){
     src += 4;
   }
 
+
+
   // 2. copy .data
   src = &_data_src;
   dst = &_data_start;
   while (dst < &_data_end) {
+    *((uintptr_t *)dst) = *((uintptr_t *)src);
+    dst += 4;
+    src += 4;
+  }
+
+  // 2.5 copy .rodata
+  src = &_rodata_src;
+  dst = &_rodata_start;
+  while (dst < &_rodata_end) {
     *((uintptr_t *)dst) = *((uintptr_t *)src);
     dst += 4;
     src += 4;
